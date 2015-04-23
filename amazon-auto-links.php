3df72168181351bf47d45f767a5e292ad30b32d9
Michael Uno <michael@michaeluno.jp>
2015-04-23 20:34:24 +0900

    Changed the version to 2.2.0 for release.

--- amazon-auto-links.php
+++ amazon-auto-links.php
@@ -5,10 +5,8 @@
 	Description:    Generates links of Amazon products just coming out today. You just pick categories and they appear even in JavaScript disabled browsers.
 	Author:         Michael Uno (miunosoft)
 	Author URI:     http://michaeluno.jp
-	Text Domain:    amazon-auto-links
-	Domain Path:    /language
 	Requirements:   WordPress >= 3.3 and PHP >= 5.2.4
-	Version:        2.2.0b01
+	Version:        2.2.0
 */
 
 /**
@@ -18,7 +16,7 @@
  */
 class AmazonAutoLinks_Commons_Base {
  
-	const Version        = '2.2.0b01';    // <--- DON'T FORGET TO CHANGE THIS AS WELL!!
+	const Version        = '2.2.0';    // <--- DON'T FORGET TO CHANGE THIS AS WELL!!
 	const Name           = 'Amazon Auto Links';
 	const Description    = 'Generates links of Amazon products just coming out today. You just pick categories and they appear even in JavaScript disabled browsers.';
 	const URI            = 'http://en.michaeluno.jp/amazon-auto-links';
