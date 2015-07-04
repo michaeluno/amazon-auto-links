7a29bf0a28e73b5eb21affda98130b4bc6b7e11d
Michael Uno <michael@michaeluno.jp>
2015-07-05 08:56:28 +0900

    Changed the version to 3.0.3 for release.

--- amazon-auto-links.php
+++ amazon-auto-links.php
@@ -5,7 +5,7 @@
 	Description:    Generates links of Amazon products just coming out today. You just pick categories and they appear even in JavaScript disabled browsers.
 	Author:         Michael Uno (miunosoft)
 	Author URI:     http://michaeluno.jp
-	Version:        3.0.2
+	Version:        3.0.3
 */
 
 /**
@@ -16,7 +16,7 @@
  */
 class AmazonAutoLinks_Registry_Base {
  
-	const VERSION        = '3.0.2';    // <--- DON'T FORGET TO CHANGE THIS AS WELL!!
+	const VERSION        = '3.0.3';    // <--- DON'T FORGET TO CHANGE THIS AS WELL!!
 	const NAME           = 'Amazon Auto Links';
 	const DESCRIPTION    = 'Generates links of Amazon products just coming out today. You just pick categories and they appear even in JavaScript disabled browsers.';
 	const URI            = 'http://en.michaeluno.jp/amazon-auto-links';
