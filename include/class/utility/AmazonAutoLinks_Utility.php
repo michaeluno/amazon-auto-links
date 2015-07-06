d42aa75c03787e7a88f0c89578a3a6836676616a
Michael Uno <michael@michaeluno.jp>
2015-07-06 09:29:50 +0900

    Fixed PHP strict standard warnings.

--- include/class/utility/AmazonAutoLinks_Utility.php
+++ include/class/utility/AmazonAutoLinks_Utility.php
@@ -121,70 +121,7 @@ class AmazonAutoLinks_Utility extends AmazonAutoLinks_Utility_XML {
     static public function isEmpty( $mValue ) {
         return ( boolean ) empty( $mValue );
     }
-    
-    /**
-     * Merges multiple multi-dimensional array recursively.
-     * 
-     * The advantage of using this method over the array unite operator or array_merge() is that it merges recursively and the null values of the preceding array will be overridden.
-     * 
-     * @since            2.0.0
-     * @static
-     * @access            public
-     * @remark            The parameters are variadic and can add arrays as many as necessary.
-     * @return            array            the united array.
-     */
-    static public function uniteArrays( $arrPrecedence, $arrDefault1 ) {
-                
-        $arrArgs  = array_reverse( func_get_args() );
-        $arrArray = array();
-        foreach( $arrArgs as $arrArg ) {
-            $arrArray = self::uniteArraysRecursive( $arrArg, $arrArray );
-        }
             
-        return $arrArray;
-        
-    }
-    /**
-     * Merges two multi-dimensional arrays recursively.
-     * 
-     * The first parameter array takes its precedence. This is useful to merge default option values. 
-     * An alternative to <em>array_replace_recursive()</em>; it is not supported PHP 5.2.x or below.
-     * 
-     * @since            2.0.0
-     * @static
-     * @access            public
-     * @remark            null values will be overwritten.     
-     * @param            array            $arrPrecedence            the array that overrides the same keys.
-     * @param            array            $arrDefault                the array that is going to be overridden.
-     * @return            array            the united array.
-     */ 
-    static public function uniteArraysRecursive( $arrPrecedence, $arrDefault ) {
-                
-        if ( is_null( $arrPrecedence ) ) { 
-            $arrPrecedence = array();
-        }
-        
-        if ( ! is_array( $arrDefault ) || ! is_array( $arrPrecedence ) ) { 
-            return $arrPrecedence;
-        }
-            
-        foreach( $arrDefault as $strKey => $v ) {
-            
-            // If the precedence does not have the key, assign the default's value.
-            if ( ! array_key_exists( $strKey, $arrPrecedence ) || is_null( $arrPrecedence[ $strKey ] ) ) {
-                $arrPrecedence[ $strKey ] = $v;
-            } else {
-                
-                // if the both are arrays, do the recursive process.
-                if ( is_array( $arrPrecedence[ $strKey ] ) && is_array( $v ) ) {
-                    $arrPrecedence[ $strKey ] = self::uniteArraysRecursive( $arrPrecedence[ $strKey ], $v );
-                }
-            
-            }
-        }
-        return $arrPrecedence;        
-    }
-        
     /**
      * Trims each delimited element of the given string with the specified delimiter. 
      * 
