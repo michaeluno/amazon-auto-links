340a3247baed0974e107d4885f5194048a882e29
Michael Uno <michael@michaeluno.jp>
2015-12-20 13:15:37 +0900

    - Fixed a bug that the Country option was missing since last several commits.

--- include/class/component/widget/contextual/form_field/AmazonAutoLinks_FormFields_Widget_ContxtualProduct.php
+++ include/class/component/widget/contextual/form_field/AmazonAutoLinks_FormFields_Widget_ContxtualProduct.php
@@ -53,9 +53,28 @@ class AmazonAutoLinks_FormFields_Widget_ContxtualProduct extends AmazonAutoLinks
                 'tip'           => array(
                     __( 'Add additional search keywords, separated by commas.', 'amazon-auto-links' ),
                     ' e.g. <code>' . __( 'laptop, desktop', 'amazon-auto-links' ) . '</code>',
-                ),                
+                ),                   
             ),     
-            
+           array(
+                'field_id'          => $sFieldIDPrefix . 'country',
+                'type'              => 'select',
+                'title'             => __( 'Country', 'amazon-auto-links' ),        
+                'label'         => array(                        
+                    'CA' => 'CA - ' . __( 'Canada', 'amazon-auto-links' ),
+                    'CN' => 'CN - ' . __( 'China', 'amazon-auto-links' ),
+                    'FR' => 'FR - ' . __( 'France', 'amazon-auto-links' ),
+                    'DE' => 'DE - ' . __( 'Germany', 'amazon-auto-links' ),
+                    'IT' => 'IT - ' . __( 'Italy', 'amazon-auto-links' ),
+                    'JP' => 'JP - ' . __( 'Japan', 'amazon-auto-links' ),
+                    'UK' => 'UK - ' . __( 'United Kingdom', 'amazon-auto-links' ),
+                    'ES' => 'ES - ' . __( 'Spain', 'amazon-auto-links' ),
+                    'US' => 'US - ' . __( 'United States', 'amazon-auto-links' ),
+                    'IN' => 'IN - ' . __( 'India', 'amazon-auto-links' ),
+                    'BR' => 'BR - ' . __( 'Brazil', 'amazon-auto-links' ),
+                    'MX' => 'MX - ' . __( 'Mexico', 'amazon-auto-links' ),
+                ),
+                'default'       => 'US',
+            ),                
         );
         
         return $_aFields;
