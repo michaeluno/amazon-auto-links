2dcdf9fd8caca9f4a8ac68e8bfed640e93b5b071
Michael Uno <michael@michaeluno.jp>
2017-01-06 13:12:14 +0900

    Corrected doc-block information.

--- include/class/component/unit/url/AmazonAutoLinks_UnitTypeLoader_url.php
+++ include/class/component/unit/url/AmazonAutoLinks_UnitTypeLoader_url.php
@@ -3,12 +3,12 @@
  * Amazon Auto Links
  * 
  * http://en.michaeluno.jp/amazon-auto-links/
- * Copyright (c) 2013-2016 Michael Uno
+ * Copyright (c) 2013-2017 Michael Uno
  * 
  */
 
 /**
- * Loads the units component.
+ * Loads the `url` unit type components.
  *  
  * @package     Amazon Auto Links
  * @since       3.3.0
