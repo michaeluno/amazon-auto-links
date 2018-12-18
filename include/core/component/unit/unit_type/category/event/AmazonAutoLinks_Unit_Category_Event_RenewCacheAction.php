0905d7606181aaf996c65151dbfa2fd2b863e70f
Michael Uno <michael@michaeluno.jp>
2018-12-19 03:35:24 +0900

    Fixed a bug that category unit caches were not deleted properly via the action link in the `Manage Units` page.

--- include/core/component/unit/unit_type/category/event/AmazonAutoLinks_Unit_Category_Event_RenewCacheAction.php
+++ include/core/component/unit/unit_type/category/event/AmazonAutoLinks_Unit_Category_Event_RenewCacheAction.php
@@ -21,7 +21,7 @@ class AmazonAutoLinks_Unit_Category_Event_RenewCacheAction extends AmazonAutoLin
     public function replyToDoAction( array $aPostIDs ) {
         foreach( $aPostIDs as $_iPostID ) {
             $_sUnitType = get_post_meta( $_iPostID, 'unit_type', true );
-            if ( 'url' !== $_sUnitType ) {
+            if ( 'category' !== $_sUnitType ) { // 3.8.1 Changed the value was `url` although the subject unit type is `category`.
                 continue;
             }
             $this->___deleteHTTPRequestCache( $_iPostID );
@@ -29,7 +29,7 @@ class AmazonAutoLinks_Unit_Category_Event_RenewCacheAction extends AmazonAutoLin
     }
         private function ___deleteHTTPRequestCache( $iPostID ) {
 
-            $_aURLs          = get_post_meta( $iPostID, 'urls', true );
+            $_aURLs          = $this->___getSubjectURLs( $iPostID );
             $_iCacheDuration = get_post_meta( $iPostID, 'cache_duration', true );
             delete_post_meta( $iPostID, '_error' );
 
@@ -38,9 +38,22 @@ class AmazonAutoLinks_Unit_Category_Event_RenewCacheAction extends AmazonAutoLin
                 $_aURLs,
                 $_iCacheDuration,
                 array(),    // http arguments
-                'url_unit_type'
+                'rss'       // 3.8.1 Changed the value from `url_unit_type` as it appears to be a mistake.
             );
             $_oHTTP->deleteCache();
 
         }
+            /**
+             * @param   $iPostID
+             * @since   3.8.1
+             * @return  array
+             */
+            private function ___getSubjectURLs( $iPostID ) {
+                $_aPageURLs          = $this->getAsArray( get_post_meta( $iPostID, 'categories', true ) );
+                $_aPageURLs          = wp_list_pluck( $_aPageURLs, 'page_url' );
+                $_aExcludingPageURLs = $this->getAsArray( get_post_meta( $iPostID, 'categories_exclude', true ) );
+                $_aExcludingPageURLs = wp_list_pluck( $_aExcludingPageURLs, 'page_url' );
+                $_aMerged            = array_merge( $_aPageURLs, $_aExcludingPageURLs );
+                return array_unique( $_aMerged );
+            }
 }
