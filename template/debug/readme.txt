93702dc804195d674ff2026c92a401f42d496e58
Michael Uno <michael@michaeluno.jp>
2019-11-22 13:14:45 +0900

    Improved the Debug template.php
    - Made it list available variables.
    - Made it more easy to read than the simple var_dump() one.

---
+++ template/debug/readme.txt
@@ -0,0 +1,8 @@
+== Change log ==
+
+= 1.1.0 =
+- Made it list available variables.
+- Improved the visual to make it more human readable.
+
+= 1.0.0 =
+- Initial release.
--- template/debug/template.php
+++ template/debug/template.php
@@ -7,12 +7,25 @@
  * $arrArgs - the user defined arguments such as image size and count etc.
  */
 
-// echo AmazonAutoLinks_Debug::get( $aArguments );
-// echo AmazonAutoLinks_Debug::get( $aOptions );
-// echo AmazonAutoLinks_Debug::get( $aProducts );
+$_aScopeVariables = array();
+$_aToDump         = array();
+foreach( get_defined_vars() as $_sKey => $_v ) {
+    // Drop variables used in this file.
+    if ( in_array( $_sKey, array( '_aScopeVariables', '_aToDump' ) ) ) {
+        continue;
+    }
+    // Drop variables kept for backward-compatibility starting with $arr.
+    if ( 'arr' === substr( $_sKey, 0, 3 ) ) {
+        continue;
+    }
+    $_aScopeVariables[ '$' . $_sKey ] = gettype( $_v );
+    $_aToDump[ '$' . $_sKey ] = $_v;
+}
+echo "<h3>Available Variables</h3>";
+AmazonAutoLinks_Debug::dump( $_aScopeVariables );
 echo "<div class='amazon-auto-links-debug'>";
-echo "<h3>Arguments</h3>";
-var_dump( $aArguments );
-echo "<h3>Products</h3>";
-var_dump( $aProducts );
+    foreach( $_aToDump as $_sVariableName => $_v ) {
+        echo "<h3>{$_sVariableName}</h3>";
+        AmazonAutoLinks_Debug::dump( $_v );
+    }
 echo "</div>";
