024629108e50072198580708d2d10a27cd64dab9
Michael Uno <michael2@michaeluno.jp>
2020-08-24 05:14:27 +0900

    Fixed some code comments.

--- uninstall.php
+++ uninstall.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Cleans up the plugin options.
+ * Cleans up the plugin options, custom post type posts, and custom database tables.
  *    
  * @package      Amazon Auto Links
  * @copyright    Copyright (c) 2013-2020, <Michael Uno>
@@ -16,11 +16,11 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
     exit;
 }
 
-/* 
+/**
  * Plugin specific constant. 
  * We are going to load the main file to get the registry class. And in the main file, 
  * if this constant is set, it will return after declaring the registry class.
- **/
+ */
 if ( ! defined( 'DOING_PLUGIN_UNINSTALL' ) ) {
     define( 'DOING_PLUGIN_UNINSTALL', true  );
 }
@@ -37,19 +37,18 @@ if ( ! class_exists( 'AmazonAutoLinks_Registry' ) ) {
     return;
 }
 
-// 0. Delete the temporary directory
+// 1. Delete the temporary directory
 $_sTempDirPath      = rtrim( sys_get_temp_dir(), '/' ) . '/' . AmazonAutoLinks_Registry::$sTempDirName;
 $_sTempDirPath_Site = $_sTempDirPath . '/' . md5( site_url() );
 if ( file_exists( $_sTempDirPath_Site ) && is_dir( $_sTempDirPath_Site ) ) {
     AmazonAutoLinks_Utility::removeDirectoryRecursive( $_sTempDirPath_Site );
-
 }
 /// Consider other sites on the same server uses the plugin
 if ( is_dir( $_sTempDirPath ) && AmazonAutoLinks_Utility::isDirectoryEmpty( $_sTempDirPath ) ) {
     AmazonAutoLinks_Utility::removeDirectoryRecursive( $_sTempDirPath );
 }
 
-// 1. Delete transients
+// 2. Delete transients
 $_aPrefixes = array(
     AmazonAutoLinks_Registry::TRANSIENT_PREFIX, // the plugin transients
     'apf_',      // the admin page framework transients
@@ -58,16 +57,15 @@ foreach( $_aPrefixes as $_sPrefix ) {
     if ( ! $_sPrefix ) { 
         continue; 
     }
-    $GLOBALS['wpdb']->query( "DELETE FROM `" . $GLOBALS['table_prefix'] . "options` WHERE `option_name` LIKE ( '_transient_%{$_sPrefix}%' )" );
-    $GLOBALS['wpdb']->query( "DELETE FROM `" . $GLOBALS['table_prefix'] . "options` WHERE `option_name` LIKE ( '_transient_timeout_%{$_sPrefix}%' )" );    
+    $GLOBALS[ 'wpdb' ]->query( "DELETE FROM `" . $GLOBALS[ 'table_prefix' ] . "options` WHERE `option_name` LIKE ( '_transient_%{$_sPrefix}%' )" );
+    $GLOBALS[ 'wpdb' ]->query( "DELETE FROM `" . $GLOBALS[ 'table_prefix' ] . "options` WHERE `option_name` LIKE ( '_transient_timeout_%{$_sPrefix}%' )" );
 }
 
-// 2. Delete options
+// 3. Delete options
 $_aOptions = get_option( AmazonAutoLinks_Registry::$aOptionKeys[ 'main' ], array() );
 $_bDelete  = isset( $_aOptions[ 'reset_settings' ][ 'reset_on_uninstall' ] ) 
     ? $_aOptions[ 'reset_settings' ][ 'reset_on_uninstall' ]
     : false;
-
 if ( ! $_bDelete ) {
     return;
 }
@@ -77,12 +75,12 @@ array_walk_recursive(
     'delete_option'   // function name
 );
 
-// 3. Delete tables
-new AmazonAutoLinks_DatabaseTableInstall( 
+// 4. Delete tables
+new AmazonAutoLinks_DatabaseTableInstall(
     false   // uninstall
 );
 
-// 4. [3.6.6+] Delete Custom Post Type Posts
+// 5. [3.6.6+] Delete Custom Post Type Posts
 foreach( AmazonAutoLinks_Registry::$aPostTypes as $_sKey => $_sPostTypeSlug ) {
     _deleteAmazonAutoLinksPosts( $_sPostTypeSlug );
 }
