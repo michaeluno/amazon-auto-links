9c9aa39a79f70cc8fdeb7a61a27c498144c4ae17
Michael Uno <michael2@michaeluno.jp>
2020-09-05 09:50:41 +0900

    Fixed a path which led to have an extra slash.

--- include/core/component/admin/help/admin_page/AmazonAutoLinks_HelpAdminPage_Help.php
+++ include/core/component/admin/help/admin_page/AmazonAutoLinks_HelpAdminPage_Help.php
@@ -26,7 +26,7 @@ class AmazonAutoLinks_HelpAdminPage_Help extends AmazonAutoLinks_AdminPage_Page_
             'order'     => 1000, // to be the last menu item
             'style'     => array(
                 AmazonAutoLinks_Registry::getPluginURL( 'asset/css/admin.css' ),
-                AmazonAutoLinks_Registry::getPluginURL( '/asset/css/code.css' ),
+                AmazonAutoLinks_Registry::getPluginURL( 'asset/css/code.css' ),
             ),
         );
     }
