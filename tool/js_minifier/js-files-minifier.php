51b0e64cbb8bb79f00eee4a2ee29f24031bad851
Michael Uno <michael2@michaeluno.jp>
2020-09-15 19:18:12 +0900

    Added the minifier script.
    This should have been added in a few latest commits.

---
+++ tool/js_minifier/js-files-minifier.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Minifies PHP files into a single file.
+ *
+ */
+
+/* Set necessary paths */
+$sTargetBaseDir		= dirname( dirname( dirname( __FILE__ ) ) );
+$aTargetDirs        = array(
+    $sTargetBaseDir . '/asset/js/',
+    $sTargetBaseDir . '/include/core/',
+);
+
+/* If accessed from a browser, exit. */
+$bIsCLI				= php_sapi_name() == 'cli';
+$sCarriageReturn	= $bIsCLI ? PHP_EOL : '<br />';
+if ( ! $bIsCLI ) { 
+    exit( 'Please run the script with a console program.' );
+}
+
+/* Include necessary files */
+require( dirname( __DIR__ ) . '/vendor/autoload.php' );
+require( dirname( __FILE__ ) . '/class/vendor/autoload.php' );
+
+/* Create a minified version of the framework. */
+echo 'Started...' . $sCarriageReturn;
+$_oGenerator = new \PHPClassMapGenerator\PHPClassMapGenerator(
+    $sTargetBaseDir,
+    $aTargetDirs,
+    '',
+    array(
+        'do_in_constructor'  => false,
+        'output_buffer'      => true,
+        'structure'          => 'PATH',
+        'search'            => [
+            'allowed_extensions'     => [ 'js' ],
+            'exclude_dir_paths'      => [],
+            'exclude_dir_names'      => [ '_del', '_bak', 'apf', 'library' ],
+            'exclude_file_names'     => [ '.min.', ],
+            'is_recursive'           => true,
+            'ignore_note_file_names' => [ 'ignore-js-min.txt' ],
+        ],
+    )
+);
+
+
+$_aFileInfoStruct = [
+    'name'     => '@name',
+    'version'  => '@version',
+];
+foreach( $_oGenerator->get() as $_sScriptPath ) {
+
+    $_aFileInfo      = $_oGenerator->getFileHeaderComment( $_sScriptPath, $_aFileInfoStruct );
+    $_sScriptName    = trim( "{$_aFileInfo[ 'name' ]} {$_aFileInfo[ 'version' ]}" );
+    $_sHeader        = $_sScriptName ? "/* {$_sScriptName} */" . PHP_EOL : '';
+    $_oMinifier      = Asika\Minifier\MinifierFactory::create('js' );
+    $_oMinifier->addFile( $_sScriptPath );
+    $_sContent       = $_sHeader . $_oMinifier->minify();
+    $_sDirPath       = dirname( $_sScriptPath );
+    $_sBaseNameWOExt = pathinfo( $_sScriptPath, PATHINFO_FILENAME );
+    $_sMinScriptPath = $_sDirPath . '/' . $_sBaseNameWOExt . '.min.js';
+    if ( file_exists( $_sMinScriptPath ) ) {
+        unlink( $_sMinScriptPath );
+    }
+    file_put_contents( $_sMinScriptPath, $_sContent, FILE_APPEND | LOCK_EX );
+    echo 'Writing to ' . $_sMinScriptPath . $sCarriageReturn;
+}
+echo 'Done!' . $sCarriageReturn;
