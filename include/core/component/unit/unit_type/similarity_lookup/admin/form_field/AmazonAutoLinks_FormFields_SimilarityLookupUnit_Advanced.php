2f12252d2b5cd9c1fc24ff4b436ce731e3006862
Michael Uno <michael2@michaeluno.jp>
2020-09-20 21:40:14 +0900

    Removed deprecated unit types.

--- include/core/component/unit/unit_type/similarity_lookup/admin/form_field/AmazonAutoLinks_FormFields_SimilarityLookupUnit_Advanced.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Provides the definitions of form fields.
- * 
- * @since           3  
- */
-class AmazonAutoLinks_FormFields_SimilarityLookupUnit_Advanced extends AmazonAutoLinks_FormFields_ItemLookupUnit_Advanced {}
