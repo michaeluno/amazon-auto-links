8d2ae2ebbb9e1ed2dbba5a7dedc2edd6768d171b
Michael Uno <michael2@michaeluno.jp>
2020-09-29 21:28:12 +0900

    Added scratches to test the test UI.

---
+++ include/core/component/test/run/scratches/test/AmazonAutoLinks_Scratch_Test.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Amazon Auto Links
+ *
+ * Generates links of Amazon products just coming out today. You just pick categories and they appear even in JavaScript disabled browsers.
+ *
+ * http://en.michaeluno.jp/amazon-auto-links/
+ * Copyright (c) 2013-2020 Michael Uno
+ */
+
+/**
+ * Scratches to deliberately cause errors to test the UI.
+ *  
+ * @package     Amazon Auto Links
+ * @since       4.3.3
+*/
+class AmazonAutoLinks_Scratch_Test extends AmazonAutoLinks_Scratch_Base {
+
+     /**
+     */
+    public function scratch_string() {
+        return __METHOD__;
+    }
+
+    /**
+     * Triggers an error.
+     */
+    public function scratch_SyntaxError() {
+        return explode( '', array() );
+    }
+
+}
---
+++ include/core/component/test/run/scratches/test/AmazonAutoLinks_Scratch_Test2.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Amazon Auto Links
+ *
+ * Generates links of Amazon products just coming out today. You just pick categories and they appear even in JavaScript disabled browsers.
+ *
+ * http://en.michaeluno.jp/amazon-auto-links/
+ * Copyright (c) 2013-2020 Michael Uno
+ */
+
+/**
+ * Scratches to deliberately cause errors to test the UI.
+ *  
+ * @package     Amazon Auto Links
+ * @since       4.3.3
+*/
+class AmazonAutoLinks_Scratch_Test2 extends AmazonAutoLinks_Scratch_Base {
+
+     /**
+     */
+    public function scratch_string2() {
+        return __METHOD__;
+    }
+
+     /**
+     */
+    public function scratch_SyntaxError2() {
+        return __METHOD__;
+    }
+
+}
