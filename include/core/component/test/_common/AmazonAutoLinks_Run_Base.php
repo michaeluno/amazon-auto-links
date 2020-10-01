16936a0e33a61348d26cd5ded7c94f294ee2fbcf
Michael Uno <michael2@michaeluno.jp>
2020-10-01 10:15:08 +0900

    Added a method to dump variable details.

--- include/core/component/test/_common/AmazonAutoLinks_Run_Base.php
+++ include/core/component/test/_common/AmazonAutoLinks_Run_Base.php
@@ -47,4 +47,22 @@ class AmazonAutoLinks_Run_Base extends AmazonAutoLinks_PluginUtility {
         $this->aOutputs[] = $sText;
     }
 
+    /**
+     * @param string sTitle
+     * @param mixed ...$mValues
+     */
+    protected function _outputDetails( $sTitle, ...$mValues ) {
+        $_aParams = func_get_args();
+        if ( 1 === func_num_args() ) {
+            $this->aOutputs[] = $this->_getDetails( $_aParams[ 0 ] );
+            return;
+        }
+        $_sTitle  = array_shift( $_aParams );
+        $_sOutput = "<h5>{$_sTitle}</h5>";
+        foreach( $mValues as $_mValue ) {
+            $_sOutput .= $this->_getDetails( $_mValue );
+        }
+        $this->aOutputs[] = $_sOutput;
+    }
+
 }
