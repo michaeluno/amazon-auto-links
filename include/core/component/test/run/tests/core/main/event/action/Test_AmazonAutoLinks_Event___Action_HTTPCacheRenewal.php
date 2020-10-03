e0ce7190fb584fb5597afc863c4108454992f9da
Michael Uno <michael2@michaeluno.jp>
2020-10-04 08:42:58 +0900

    Added a test class (Not adding tests yet).

---
+++ include/core/component/test/run/tests/core/main/event/action/Test_AmazonAutoLinks_Event___Action_HTTPCacheRenewal.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Amazon Auto Links
+ *
+ * Generates links of Amazon products just coming out today. You just pick categories and they appear even in JavaScript disabled browsers.
+ *
+ * http://en.michaeluno.jp/amazon-auto-links/
+ * Copyright (c) 2013-2020 Michael Uno
+ */
+
+/**
+ * Tests the class, `AmazonAutoLinks_Event___Action_HTTPCacheRenewal`.
+ *
+ * @package Amazon Auto Links
+ * @since   4.3.4
+ * @see     AmazonAutoLinks_Event___Action_HTTPCacheRenewal
+ * @tags    http
+*/
+class Test_AmazonAutoLinks_Event___Action_HTTPCacheRenewal extends AmazonAutoLinks_UnitTest_Base {
+
+
+
+}
