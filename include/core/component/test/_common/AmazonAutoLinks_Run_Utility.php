3abc330c52a604ab3531de7fede710dd9b9f3e1e
Michael Uno <michael2@michaeluno.jp>
2020-10-08 16:09:44 +0900

    Added the `$aRemoveTags` parameter to the `getHTMLBody()` method.

--- include/core/component/test/_common/AmazonAutoLinks_Run_Utility.php
+++ include/core/component/test/_common/AmazonAutoLinks_Run_Utility.php
@@ -18,15 +18,16 @@ class AmazonAutoLinks_Run_Utility extends AmazonAutoLinks_PluginUtility {
 
     /**
      * @param  string $sHTML
+     * @param  array  $aRemoveTags  HTML tags to remove.
      * @return string
      * @since  4.3.4
      */
-    static public function getHTMLBody( $sHTML ) {
+    static public function getHTMLBody( $sHTML, $aRemoveTags=array( 'script', 'style' ) ) {
 
         $_oDOM    = new AmazonAutoLinks_DOM;
         $_oDoc    = $_oDOM->loadDOMFromHTML( $sHTML );
         $_oXPath  = new DOMXPath( $_oDoc );
-        $_oDOM->removeTags( $_oDoc, array( 'script', 'style' ) );
+        $_oDOM->removeTags( $_oDoc, $aRemoveTags );
         $_noNode  = $_oXPath->query( './/body' )->item( 0 );
         return $_oDOM->getInnerHTML( $_noNode );
 
