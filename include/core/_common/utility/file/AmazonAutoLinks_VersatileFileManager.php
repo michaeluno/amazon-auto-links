41286d03be4d85055954ddb6b3ce558417cc3687
Michael Uno <michael2@michaeluno.jp>
2020-10-17 04:24:13 +0900

    Added the ability to modify file contents.
    This is by adding a public method, `set()` and `get()` which modify/retrieve file contents.
    
    Also, refactored code.

--- include/core/_common/utility/file/AmazonAutoLinks_VersatileFileManager.php
+++ include/core/_common/utility/file/AmazonAutoLinks_VersatileFileManager.php
@@ -28,41 +28,89 @@ class AmazonAutoLinks_VersatileFileManager {
 
     protected $_iTimeout;
 
+    /**
+     * @var   string
+     * @sicne 4.3.4  Stores the file path.
+     */
+    protected $_sFilePath = '';
+
+    /**
+     * Sets up properties and create a temp directory.
+     *
+     * @param string  $sIdentifier      A string to identify the lock file.
+     * @param integer $iTimeout         Timeout in seconds.
+     * @param string  $sFileNamePrefix  A prefix to prepend to the file name.
+     */
     public function __construct( $sIdentifier, $iTimeout=30, $sFileNamePrefix='AALTemp_' ) {
         $this->_sIdentifier       = $sIdentifier;
-        $this->_sTempDirPath      = AmazonAutoLinks_Registry::getPluginSiteTempDirPath();
+        $this->_sTempDirPath      = AmazonAutoLinks_Registry::getPluginSiteTempDirPath() . '/versatile';
         $this->_iTimeout          = $iTimeout;
         $this->_sFileNamePrefix   = $sFileNamePrefix;
+        $this->_sFilePath         = $this->___getFilePath();
+        if ( ! is_dir( $this->_sTempDirPath ) ) {
+            mkdir( $this->_sTempDirPath, 0777, true );
+        }
+    }
+        /**
+         * @remark Consider the file resides in the server's shared temporary directory.
+         * @return string
+         */
+        private function ___getFilePath() {
+            return $this->_sTempDirPath . '/'
+                . $this->_sFileNamePrefix . md5(site_url() . $this->_sIdentifier )
+                . '.txt';
+        }
+
+    /**
+     * Returns the file contents if it is not expired.
+     * @since  4.3.4
+     */
+    public function get() {
+        return $this->isAlive()
+            ? file_get_contents( $this->_sFilePath )
+            : '';
     }
 
     /**
-     * @return bool
+     * @param  string $sText
+     * @return false|int
+     * @since  4.3.4
+     */
+    public function set( $sText ) {
+        return file_put_contents( $this->_sFilePath, $sText, LOCK_EX );
+    }
+
+    /**
+     * @return boolean
      */
     public function isLocked() {
-        if ( $this->___isAlive() ) {
+        if ( $this->isAlive() ) {
             return true;
         }
         // At this point, the file does not exist or timed out
-        $this->___set();
+        $this->___touch();
         return false;
     }
 
     /**
+     * Checks if the lock file is expired or not.
+     *
      * As WP_Shadow is implemented, occasionally the action may run in multiple separate page loads at almost the exact same time, one in wp-cron.php and index.php.
      * If that happens, the action runs twice.
-     * This method ensures it only runs once by checking a lock file.
-     * Checks 30 seconds.
-     * @since   3.7.7
-     * @since   4.2.3       Changed the name from `exist()` to `___isAlive()`
-     * @return boolean  True if the file is not timed out; otherwise, false.
+     *
+     * This method ensures that it only runs once by checking a lock file.
+     *
+     * @since  3.7.7
+     * @since  4.2.3    Changed the name from `exist()` to `___isAlive()`
+     * @since  4.3.4    Changed the scope to public and renamed from `___isAlive()`.
+     * @return boolean  `true` if the file is not timed out; otherwise, `false`.
      */
-    private function ___isAlive() {
+    public function isAlive() {
 
-        $_sLockFilePath = $this->___getActionLockFilePath();
-        if ( ! file_exists( $_sLockFilePath ) ) {
+        if ( ! file_exists( $this->_sFilePath ) ) {
             return false; // not alive (not created yet)
         }
-        $_iModifiedTime  = ( integer ) filemtime( $_sLockFilePath );
+        $_iModifiedTime  = ( integer ) filemtime( $this->_sFilePath );
         if ( $_iModifiedTime + $this->_iTimeout > time() ) {
             // the file is not timed-out yet
             return true; // alive
@@ -73,35 +121,26 @@ class AmazonAutoLinks_VersatileFileManager {
     }
 
     /**
-     * Sets the lock file.
+     * Touches a lock file.
+     *
+     * If a file does not exist, it will be crated.
+     *
      * @since   3.7.7
      * @since   4.2.3   Changed the scope to private form public.
      * @since   4.2.3   Changed the ame from `set()` to `___set()`.
+     * @since   4.3.4   Renamed from `___set()`.
      */
-    private function ___set() {
+    private function ___touch() {
 
-        if ( ! is_dir( $this->_sTempDirPath ) ) {
-            mkdir( $this->_sTempDirPath, 0777, true );
-        }
-        $_sLockFilePath = $this->___getActionLockFilePath();
-        if ( file_exists( $_sLockFilePath ) ) {
+        if ( file_exists( $this->_sFilePath ) ) {
             // Update the modification time
-            touch( $_sLockFilePath );
+            touch( $this->_sFilePath );
             return;
         }
 
         // At this point, the file does not exist so creat it.
-        file_put_contents( $_sLockFilePath, microtime( true ), LOCK_EX );
+        $this->set( microtime( true ) );
 
     }
-        /**
-         * @remark Consider the file resides in the server's shared temporary directory.
-         * @return string
-         */
-        private function ___getActionLockFilePath() {
-            return $this->_sTempDirPath . '/'
-                . $this->_sFileNamePrefix . md5(site_url() . $this->_sIdentifier )
-                . '.txt';
-        }
 
 }
--- include/core/_common/utility/file/AmazonAutoLinks_VersatileFileManager_DeleteMode.php
+++ include/core/_common/utility/file/AmazonAutoLinks_VersatileFileManager_DeleteMode.php
@@ -16,31 +16,43 @@
 class AmazonAutoLinks_VersatileFileManager_DeleteMode extends AmazonAutoLinks_VersatileFileManager {
 
     /**
+     * Returns the file contents if it is not expired.
+     * @since  4.3.4
+     */
+    public function get() {
+        $_sFileContent = parent::get();
+        if ( ! $this->isAlive() ) {
+            add_action( 'shutdown', array( $this, 'replyToCleanFiles' ) );
+        }
+        return $_sFileContent;
+    }
+
+    /**
      * @return bool
      */
     public function isLocked() {
-        $_bLocked = parent::isLocked();
-        if ( $_bLocked ) {
+        if ( parent::isLocked() ) {
             return true;
         }
         add_action( 'shutdown', array( $this, 'replyToCleanFiles' ) );
         return false;
     }
 
-        /**
-         * The created file will not be deleted because in the situation there are two simultaneously spawned actions,
-         * at the time that the one finishes immediately and after that the other starts, the other one thinks it is not locked
-         * and does the same routine again, which is redundant and consumes the server resource.
-         * So here just cleans up left files with the set timeout.
-         */
-        public function replyToCleanFiles() {
-            $_sPattern = $this->_sTempDirPath . DIRECTORY_SEPARATOR
-                . $this->_sFileNamePrefix . "*.txt";
-            foreach ( glob( $_sPattern ) as $_sFilePath ) {
-                if ( time() - filectime( $_sFilePath ) > $this->_iTimeout ) {
-                    unlink( $_sFilePath );
-                }
+    /**
+     * Cleans up left files with the set timeout.
+     *
+     * This is to cover cases of more than one simultaneously spawned actions;
+     * at the time that the one finishes immediately and after that the other starts, the other one thinks it is not locked
+     * and does the same routine again, which is redundant and consumes the server resource.
+     */
+    public function replyToCleanFiles() {
+        $_sPattern = $this->_sTempDirPath . DIRECTORY_SEPARATOR
+            . $this->_sFileNamePrefix . "*.txt";
+        foreach ( glob( $_sPattern ) as $_sFilePath ) {
+            if ( time() - filectime( $_sFilePath ) > $this->_iTimeout ) {
+                unlink( $_sFilePath );
             }
         }
+    }
 
 }
