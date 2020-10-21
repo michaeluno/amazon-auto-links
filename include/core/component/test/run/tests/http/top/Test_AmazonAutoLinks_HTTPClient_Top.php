4452b85c1bcb67b64f575b781d4b6bd7f31960a8
Michael Uno <michael2@michaeluno.jp>
2020-10-21 14:02:12 +0900

    Add tests.
    To check whether HTTP requests are blocked or not.

---
+++ include/core/component/test/run/tests/http/top/Test_AmazonAutoLinks_HTTPClient_Top.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Amazon Auto Links
+ *
+ * Generates links of Amazon products just coming out today. You just pick categories and they appear even in JavaScript disabled browsers.
+ *
+ * http://en.michaeluno.jp/amazon-auto-links/
+ * Copyright (c) 2013-2020 Michael Uno
+ */
+
+/**
+ * Tests accessing Amazon top pages.
+ *
+ * @package Amazon Auto Links
+ * @since   4.3.5
+ * @see     AmazonAutoLinks_HTTPClient
+ * @tags    http
+*/
+class Test_AmazonAutoLinks_HTTPClient_Top extends Test_AmazonAutoLinks_HTTPClient_BestSellers {
+
+    /**
+     * @tags top
+     */
+    public function test_allLocales() {
+        foreach( AmazonAutoLinks_Locales::getLocaleObjects() as $_sLocale => $_oLocale ) {
+            $this->_testUnblocked( $_oLocale->getMarketPlaceURL(), $_sLocale );
+        }
+    }
+
+}
