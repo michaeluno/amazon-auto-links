4c8ca5871e083fbb6223c75807763dfbed3a6b0a
Michael Uno <michael2@michaeluno.jp>
2020-11-11 00:58:23 +0900

    [Scratch] Added a scratch to display HTTP request caches by cache name.

--- include/core/component/test/run/scratches/database/table/aal_request_cache/AmazonAutoLinks_Scratch_Database_aal_request_cache.php
+++ include/core/component/test/run/scratches/database/table/aal_request_cache/AmazonAutoLinks_Scratch_Database_aal_request_cache.php
@@ -26,4 +26,18 @@ class AmazonAutoLinks_Scratch_Database_aal_request_cache extends AmazonAutoLinks
         return $_oTable->getTableStatus();
     }
 
+    /**
+     * @purpose Shows cache with the given argument. Set a cache name in the first argument input field.
+     * @tags cache
+     * @throws Exception
+     */
+    public function scratch_showCache() {
+        $_sCacheName = trim( func_get_arg( 0 ) );
+        if ( ! $_sCacheName ) {
+            throw new Exception( 'A cache name is not passed. Set a cache name in the first argument input field.' );
+        }
+        $_oTable = new AmazonAutoLinks_DatabaseTable_aal_request_cache;
+        $this->_outputDetails( $_oTable->getCache( $_sCacheName ) );
+    }
+
 }
