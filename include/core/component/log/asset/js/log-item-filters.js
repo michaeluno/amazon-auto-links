dc09e9ce7134cbf6c3b49801faeb9ad645f81f46
Michael Uno <michael2@michaeluno.jp>
2020-11-11 23:54:54 +0900

    [Bug][UI] Fix a bug that the log container disappear after typing on the Include or Exclude input fields.

--- include/core/component/log/asset/js/log-item-filters.js
+++ include/core/component/log/asset/js/log-item-filters.js
@@ -6,7 +6,7 @@
  * http://en.michaeluno.jp/amazon-auto-links/
  * Copyright (c) 2013-2020 Michael Uno
  * @name Log Item Filters
- * @version 1.0.2
+ * @version 1.0.3
  */
 (function($){
 
@@ -63,7 +63,8 @@
                 }
             } );
 
-            $( '.log' ).replaceWith( oLogClone );
+            $( '.log' ).replaceWith( oLogClone.show() );
+            _fixFormLayout();
 
             // Accordion
             $( '.log-item-head' ).click( function() {
