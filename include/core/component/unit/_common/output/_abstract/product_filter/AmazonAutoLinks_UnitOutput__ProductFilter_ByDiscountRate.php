233f3d81c85310d20de5f3223b7997254b9c2b9d
Michael Uno <michael2@michaeluno.jp>
2020-11-27 10:32:09 +0900

    [Big] Fix a bug that Discount Rate advanced product filter unit option did not function for the category unit type.
    This fixes:
     - a flaw in the logic that checks formatted string values to calculate a discount percentage with the wrong column names.
     - a case that a discount price amount is zero which caused a discount percentage is calculated as 100%.

--- include/core/component/unit/_common/output/_abstract/product_filter/AmazonAutoLinks_UnitOutput__ProductFilter_ByDiscountRate.php
+++ include/core/component/unit/_common/output/_abstract/product_filter/AmazonAutoLinks_UnitOutput__ProductFilter_ByDiscountRate.php
@@ -30,50 +30,25 @@ class AmazonAutoLinks_UnitOutput__ProductFilter_ByDiscountRate extends AmazonAut
      * @param       array   $aProduct
      * @param       array   $aRow
      * @param       array   $aRowIdentifier
+     * ### Structure
+     * - locale (string)
+     * - asin (string)
+     * - associate_id (string)
      * @callback    add_filter      aal_filter_unit_each_product_with_database_row
      * @return      array           The filtered product definition array. If it does not meet the user-set criteria, an empty array will be returend to be filtered out.
      */
     public function replyToFilterProduct( $aProduct, $aRow, $aRowIdentifier ) {
 
-        // Case: Already set. This can occur with feed units.
-        if ( isset( $aProduct[ 'price_amount' ], $aProduct[ 'discounted_price_amount' ], $aProduct[ 'lowest_new_price_amount' ] ) && is_numeric( $aProduct[ 'price_amount' ] ) ) {
-
-            $_iPrice      = ( integer ) $aProduct[ 'price_amount' ];
-            $_iLowestNew  = ( integer ) $aProduct[ 'discounted_price_amount' ];
-            $_iDiscounted = ( integer ) $aProduct[ 'lowest_new_price_amount' ];
-
-        } else {
-
-            $_oRow = new AmazonAutoLinks_UnitOutput___Database_Product(
-                $aRowIdentifier[ 'asin' ],
-                $aRowIdentifier[ 'locale' ],
-                $aRowIdentifier[ 'associate_id' ],
-                $aRow,
-                $this->_oUnitOutput->oUnitOption
-            );
-
-            $_inPrice      = $_oRow->getCell( 'price_amount' );
-            $_inDiscounted = $_oRow->getCell( 'discounted_price_amount' );
-            $_inLowestNew  = $_oRow->getCell( 'lowest_new_price_amount' );
-
-            // If the data is not ready, do not show them.
-            if ( ! $this->___isProductReady( $_inPrice, $_inDiscounted, $_inLowestNew ) ) {
-                return array();
-            }
-
-            $_iPrice      = ( integer ) $_inPrice;
-            $_iLowestNew  = ( integer ) $_inLowestNew;
-            $_iDiscounted = ( integer ) $_inDiscounted;
-
+        $_bSet                  = $this->___setVariablesOfPrice( $aProduct, $aRow, $aRowIdentifier, $_iPrice, $_iLowestNew, $_iDiscounted );
+        if ( ! $_bSet ) {
+            return array();
         }
-
-        $_iLowest               = min( $_iLowestNew, $_iDiscounted );
+        $_iLowest               = $_iDiscounted ? min( $_iLowestNew, $_iDiscounted ) : $_iLowestNew;
         $_dDiscountPercentage   = $_iPrice
             ? 100 - round( ( $_iLowest / $_iPrice ) * 100, 2 )
             : 0;
         $_dAcceptedDiscountRate = ( double ) $this->_oUnitOutput->oUnitOption->get( '_filter_by_discount_rate', 'amount' );
-        $_sCase                 = $this->_oUnitOutput->oUnitOption->get( '_filter_by_discount_rate', 'case' );
-        switch ( $_sCase ) {
+        switch ( $this->_oUnitOutput->oUnitOption->get( '_filter_by_discount_rate', 'case' ) ) {
             case 'below':
                 return ( $_dAcceptedDiscountRate >= $_dDiscountPercentage )
                     ? $aProduct
@@ -86,6 +61,51 @@ class AmazonAutoLinks_UnitOutput__ProductFilter_ByDiscountRate extends AmazonAut
         }
 
     }
+        /**
+         * @param  array    $aProduct
+         * @param  array    $aRow
+         * @param  array    $aRowIdentifier
+         * @param  integer &$_iPrice
+         * @param  integer &$_iLowestNew
+         * @param  integer &$_iDiscounted
+         * @return boolean  true if variables are set. Otherwise, false.
+         * @since  4.2.2
+         */
+        private function ___setVariablesOfPrice( $aProduct, $aRow, $aRowIdentifier, &$_iPrice, &$_iLowestNew, &$_iDiscounted ) {
+
+            // Case: Already set. This can occur with feed units.
+            if ( isset( $aProduct[ 'price_amount' ], $aProduct[ 'discounted_price_amount' ], $aProduct[ 'lowest_new_price_amount' ] ) && is_numeric( $aProduct[ 'price_amount' ] ) ) {
+
+                $_iPrice      = ( integer ) $aProduct[ 'price_amount' ];
+                $_iLowestNew  = ( integer ) $aProduct[ 'discounted_price_amount' ];
+                $_iDiscounted = ( integer ) $aProduct[ 'lowest_new_price_amount' ];
+                return true;
+            }
+
+            $_oRow = new AmazonAutoLinks_UnitOutput___Database_Product(
+                $aRowIdentifier[ 'asin' ],
+                $aRowIdentifier[ 'locale' ],
+                $aRowIdentifier[ 'associate_id' ],
+                $aRow,
+                $this->_oUnitOutput->oUnitOption
+            );
+
+            // The database column names and the product array keys are different
+            $_inPrice      = $_oRow->getCell( 'price' );                   // corresponds to `price_amount` in the product array
+            $_inDiscounted = $_oRow->getCell( 'discounted_price' );        // corresponds to `discounted_price_amount` in the product array
+            $_inLowestNew  = $_oRow->getCell( 'lowest_new_price' );        // corresponds to `lowest_new_price_amount` in the product array
+
+            // If the data is not ready, do not show them.
+            if ( ! $this->___isProductReady( $_inPrice, $_inDiscounted, $_inLowestNew ) ) {
+                return false;
+            }
+
+            $_iPrice      = ( integer ) $_inPrice;
+            $_iLowestNew  = ( integer ) $_inLowestNew;
+            $_iDiscounted = ( integer ) $_inDiscounted;
+            return true;
+
+        }
 
         /**
          * @param   null|integer $_inPrice
