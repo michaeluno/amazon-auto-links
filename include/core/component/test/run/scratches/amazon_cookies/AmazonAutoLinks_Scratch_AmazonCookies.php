7adab5b66e4f8c1ab03b612fa47a70cbaf6f3579
Michael Uno <michael2@michaeluno.jp>
2020-12-20 04:38:30 +0900

    [Scratch] Add a scratch to show stored Amazon cookies.

---
+++ include/core/component/test/run/scratches/amazon_cookies/AmazonAutoLinks_Scratch_AmazonCookies.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Amazon Auto Links
+ *
+ * Generates links of Amazon products just coming out today. You just pick categories and they appear even in JavaScript disabled browsers.
+ *
+ * http://en.michaeluno.jp/amazon-auto-links/
+ * Copyright (c) 2013-2020 Michael Uno
+ */
+
+/**
+ * A scratch class for Amazon cookies.
+ *  
+ * @package     Amazon Auto Links
+ * @since       4.5.0
+*/
+class AmazonAutoLinks_Scratch_AmazonCookies extends AmazonAutoLinks_Scratch_Base {
+
+    /**
+     * @purpose Shows saved cookies of a specified locale.
+     * @tags   cookies
+     * @throws Exception
+     */
+    public function scratch_saveFileLogToDatabase() {
+        $_aParameters = func_get_args() + array( '' );
+        $_sLocale     = $_aParameters[ 0 ];
+        if ( ! $_sLocale ) {
+            throw new Exception( 'Pass a locale code to the first parameter.' );
+        }
+        $_oLocale        = new AmazonAutoLinks_Locale( $_sLocale );
+        $_oAmazonCookies = new AmazonAutoLinks_Locale_AmazonCookies( $_oLocale->get() );
+        $this->_outputDetails( $_sLocale, $this->getCookiesToParse( $_oAmazonCookies->get() ) );
+    }
+
+
+}
