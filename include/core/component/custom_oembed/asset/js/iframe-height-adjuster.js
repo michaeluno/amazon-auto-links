b0b7ecaba2b3b9115fedd629aba1e352e39b661c
Michael Uno <michael2@michaeluno.jp>
2020-12-22 01:34:25 +0900

    Fix a bug that height of embedded outputs with iframe is not adjusted properly when multiple instances are present.

--- include/core/component/custom_oembed/asset/js/iframe-height-adjuster.js
+++ include/core/component/custom_oembed/asset/js/iframe-height-adjuster.js
@@ -3,6 +3,9 @@
  * Original idea from bypass-iframe-height-limit plugin by Justin Carboneau
  * Adapted from original /wp-includes/js/wp-embed.js
  * @see https://medium.com/@wlarch/overwrite-and-bypass-wordpress-iframe-height-dimension-limit-using-javascript-9d5035c89e37
+ * @name iframe Height Adjuster
+ * @version 1.1.0
+ * @remark Modified by Michael Uno
  */
 (function(window, document) {
     'use strict';
@@ -39,35 +42,45 @@
 		// Select all iframes
         var iframes = document.querySelectorAll('iframe[data-secret="' + data.secret + '"]'),
             i, source;
-
+        var _cssRules = '';
         for (i = 0; i < iframes.length; i++) {
+
             source = iframes[i];
 
-            if (e.source !== source.contentWindow) {
+            if ( e.source !== source.contentWindow ) {
+                continue;
+            }
+            if ( 'height' !== data.message ) {
                 continue;
             }
 
-            if ('height' === data.message) {
-                // wp-embed.js clears any added inline styls, that's why we need to create a style element
-                var styleElem = document.getElementById('aalEmbed-style-' + source.getAttribute('data-secret'));
-                var css = 'iframe.wp-embedded-content.aal-embed[data-secret="' + source.getAttribute('data-secret') +
-                    '"] { height: ' + parseInt(data.value, 10) + 'px; }';
+            var _iHeight = parseInt(data.value, 10 );
+            var _attrID = source.getAttribute( 'id' );
+            _attrID = _attrID ? _attrID : 'iframe-' + source.getAttribute('data-secret') + '-' + i;
+            source.setAttribute( 'id', _attrID );
+            source.setAttribute( 'height', _iHeight.toString() );
+            var css = '#' + _attrID + ' { height: ' + _iHeight + 'px; } ';
+            _cssRules += css;
 
-                if (styleElem) {
-                    styleElem.innerHTML = css;
-                } else {
-                    var head = document.head || document.getElementsByTagName('head')[0],
-                        style = document.createElement('style');
+        } // end for()
 
-                    style.type = 'text/css';
-                    style.id = 'aalEmbed-style-' + source.getAttribute('data-secret');
-                    style.appendChild(document.createTextNode(css));
-
-                    head.appendChild(style);
-                }
-            }
-
+        if ( ! _cssRules ) {
+            return;
         }
+        // Now add stylesheet
+        // wp-embed.js clears any added inline styles, that's why we need to create a style element
+        var _styleElem = document.getElementById('aalEmbed-style-' + data.secret );
+        if ( _styleElem ) {
+            _styleElem.innerHTML = _cssRules;
+            return;
+        }
+        var head  = document.head || document.getElementsByTagName( 'head' )[ 0 ],
+            style = document.createElement( 'style' );
+
+        style.type = 'text/css';
+        style.id = 'aalEmbed-style-' + data.secret;
+        style.appendChild( document.createTextNode( _cssRules ) );
+        head.appendChild( style );
     };
 
     if (supportedBrowser) {
