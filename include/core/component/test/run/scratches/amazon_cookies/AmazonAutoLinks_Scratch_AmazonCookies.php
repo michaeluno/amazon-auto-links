e53a01b987471a6830cb06a4e69fded40aa0e721
Michael Uno <michael2@michaeluno.jp>
2020-12-25 05:37:06 +0900

    Scratch: add additional parameters to specify which types to display.

--- include/core/component/test/run/scratches/amazon_cookies/AmazonAutoLinks_Scratch_AmazonCookies.php
+++ include/core/component/test/run/scratches/amazon_cookies/AmazonAutoLinks_Scratch_AmazonCookies.php
@@ -26,20 +26,28 @@ class AmazonAutoLinks_Scratch_AmazonCookies extends AmazonAutoLinks_Scratch_Base
         $_aParameters = func_get_args() + array( '' );
         $_sLocale     = $_aParameters[ 0 ];
         if ( ! $_sLocale ) {
-            throw new Exception( 'Pass a locale code to the first parameter.' );
+            throw new Exception( 'Pass a locale code to the first parameter. For the rest of the parameters, specify the types to skip from either of "file", "transient", and "direct".' );
         }
         $this->_output( "<strong>$_sLocale</strong>" );
 
-        $_oVersatileCookies = new AmazonAutoLinks_VersatileFileManager_AmazonCookies( $_sLocale );
-        $this->_outputDetails( "File", $this->getCookiesToParse( $_oVersatileCookies->get() ) );
+        array_shift( $_aParameters );
+        $_aTypes = array_map('strtolower', $_aParameters );
+        if ( ! in_array( 'file', $_aTypes, true ) ) {
+            $_oVersatileCookies = new AmazonAutoLinks_VersatileFileManager_AmazonCookies( $_sLocale );
+            $this->_outputDetails( "File", $this->getCookiesToParse( $_oVersatileCookies->get() ) );
+        }
 
-        $_sTransientPrefix     = AmazonAutoLinks_Registry::TRANSIENT_PREFIX;
-        $_sTransientKey        = "_transient_{$_sTransientPrefix}_cookies_{$_sLocale}";
-        $this->_outputDetails( "Transient", $this->getCookiesToParse( $this->getAsArray( get_option( $_sTransientKey, array() ) ) ) );
+        if ( ! in_array( 'transient', $_aTypes, true ) ) {
+            $_sTransientPrefix = AmazonAutoLinks_Registry::TRANSIENT_PREFIX;
+            $_sTransientKey    = "_transient_{$_sTransientPrefix}_cookies_{$_sLocale}";
+            $this->_outputDetails( "Transient", $this->getCookiesToParse( $this->getAsArray( get_option( $_sTransientKey, array() ) ) ) );
+        }
 
-        $_oLocale        = new AmazonAutoLinks_Locale( $_sLocale );
-        $_oAmazonCookies = new AmazonAutoLinks_Locale_AmazonCookies( $_oLocale->get() );
-        $this->_outputDetails( "Direct", $this->getCookiesToParse( $_oAmazonCookies->get() ) );
+        if ( ! in_array( 'direct', $_aTypes, true ) ) {
+            $_oLocale        = new AmazonAutoLinks_Locale( $_sLocale );
+            $_oAmazonCookies = new AmazonAutoLinks_Locale_AmazonCookies( $_oLocale->get() );
+            $this->_outputDetails( "Direct", $this->getCookiesToParse( $_oAmazonCookies->get() ) );
+        }
 
     }
 
