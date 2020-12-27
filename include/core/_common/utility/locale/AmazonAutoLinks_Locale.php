michaeluno/amazon-auto-links

commit d9b3a8342375ed9a641a5b1e68889e9b6d592f3b

Feature: add the ability to utilize Web Page Dumper to help HTTP requests
Often HTTP requests to Amazon sites are blocked by captcha and with real browser cookies it can be avoided.

include/core/_common/utility/locale/AmazonAutoLinks_Locale_Base.php
@@ -12,7 +12,7 @@
  *
  * @since       4.3.4
  */
-abstract class AmazonAutoLinks_Locale_Base {
+abstract class AmazonAutoLinks_Locale_Base extends AmazonAutoLinks_PluginUtility {
 
     /**
      * The locale code.
@@ -206,9 +206,53 @@ abstract class AmazonAutoLinks_Locale_Base {
      * @since  4.3.4
      */
     public function getHTTPRequestCookies( $sLanguage='' ) {
+
+        $_aCachedCookies = $this->___getCookieTransient( $sLanguage );
+        if ( ! empty( $_aCachedCookies ) ) {
+            return $_aCachedCookies;
+        }
         $_oCookieGetter = new AmazonAutoLinks_Locale_AmazonCookies( $this, $sLanguage );
-        return $_oCookieGetter->get();
+        $_aCookies = $_oCookieGetter->get();
+        $this->___setCookieTransient( $_aCookies, $_oCookieGetter );
+        return $_aCookies;
+
     }
+        /**
+         * @param  string $sLanguage
+         * @return WP_Http_Cookie[]
+         * @since  4.3.4
+         * @since  4.5.0    Moved from AmazonAutoLinks_Locale_AmazonCookies
+         */
+        private function ___getCookieTransient( $sLanguage ) {
+            $_sTransientPrefix     = AmazonAutoLinks_Registry::TRANSIENT_PREFIX;
+            $_sTransientKey        = "_transient_{$_sTransientPrefix}_cookies_{$this->sSlug}";
+            $_sTransientKeyTimeout = "_transient_timeout_{$_sTransientPrefix}_cookies_{$this->sSlug}";
+            $_aCookies             = $this->getAsArray( get_option( $_sTransientKey, array() ) );
+            $_aOldCookies          = $_aCookies;
+            $_iLifespan            = ( integer ) get_option( $_sTransientKeyTimeout, 0 );
+            if ( $this->isExpired( $_iLifespan ) ) {
+                $_aCookies = array();
+            }
+            return $this->getAsArray( apply_filters( 'aal_filter_amazon_cookies_cache', $_aCookies, $_iLifespan, $this, $sLanguage, $_aOldCookies ) );
+        }
+        /**
+         * Sets an option looking like a transient in the options table.
+         * The data is stored as an option but with the transient name.
+         * This is to enable the autoload option but with an expiration time.
+         * By using set_transient(), if an expiration time is given, the autoload option will be disabled.
+         * @param  array   $aCookies
+         * @param  AmazonAutoLinks_Locale_AmazonCookies $oCookieGetter
+         * @return boolean
+         * @since  4.3.4
+         * @since  4.5.0    Moved from AmazonAutoLinks_Locale_AmazonCookies
+         */
+        private function ___setCookieTransient( array $aCookies, AmazonAutoLinks_Locale_AmazonCookies $oCookieGetter ) {
+            $_sTransientPrefix = AmazonAutoLinks_Registry::TRANSIENT_PREFIX;
+            $_sNameTimeout     = "_transient_timeout_{$_sTransientPrefix}_cookies_{$this->sSlug}";
+            $_sName            = "_transient_{$_sTransientPrefix}_cookies_{$this->sSlug}";
+            update_option( $_sNameTimeout, time() + $oCookieGetter->iCacheDuration );
+            return update_option( $_sName, $aCookies );
+        }
 
     /**
      * @remark The supported locales: US, CA, FR, DE, UK, JP.

include/core/main/event/action/AmazonAutoLinks_Main_Event_Action_AmazonCookiesRenewal.php
@@ -1,0 +1,37 @@
+<?php
+/**
+ * Amazon Auto Links
+ *
+ * Generates links of Amazon products just coming out today. You just pick categories and they appear even in JavaScript disabled browsers.
+ *
+ * http://en.michaeluno.jp/amazon-auto-links/
+ * Copyright (c) 2013-2020 Michael Uno
+ */
+
+/**
+ * Handles renewing Amazon site cookies.
+ *
+ * @since        4.5.0
+ */
+class AmazonAutoLinks_Main_Event_Action_AmazonCookiesRenewal extends AmazonAutoLinks_PluginUtility {
+
+    /**
+     * Sets up hooks.
+     */
+    public function __construct() {
+        add_action( 'aal_action_renew_amazon_cookies', array( $this, 'replyToRenewAmazonCookies' ), 10, 2 );
+    }
+
+    /**
+     * @param  string $sLocale
+     * @param  string $sLanguage
+     * @since  4.0.0
+     */
+    public function replyToRenewAmazonCookies( $sLocale, $sLanguage ) {
+        $_oLocaleSelector = new AmazonAutoLinks_Locale( $sLocale );
+        $_oLocale         = $_oLocaleSelector->get();
+        $_oCookieGetter   = new AmazonAutoLinks_Locale_AmazonCookies( $_oLocale, $sLanguage );
+        $_oCookieGetter->get();
+    }
+
+}

asset/log/4.5.0.rm
@@ -1,1 +1,2 @@
+- Added the ability to utilize Web Page Dumper to help HTTP requests. 
 - Fixed an issue that the `Link Style Query Key` general option could be empty, which resulted in unit RSS and JSON feeds broken.	

include/core/_common/utility/locale/cookie/AmazonAutoLinks_Locale_AmazonCookies.php
@@ -40,18 +40,6 @@ class AmazonAutoLinks_Locale_AmazonCookies extends AmazonAutoLinks_PluginUtility
     public $iCacheDuration = 604800;    // ( 60 * 60 * 24 * 7 ) : 7 days
 
     /**
-     * @var   string
-     * @since 4.3.4
-     */
-    public $sTransientKey  = '';
-
-    /**
-     * @var   string
-     * @since 4.3.4
-     */
-    public $sTransientKeyTimeout = '';
-
-    /**
      * The request type passed to `AmazonAutoLinks_HTTPClient`.
      *
      * With this set, the cookies will not be suppressed by filters since the `amazon_cookie` type is excepted.
@@ -71,9 +59,6 @@ class AmazonAutoLinks_Locale_AmazonCookies extends AmazonAutoLinks_PluginUtility
         $this->oLocale              = $oLocale;
         $this->sLocale              = $oLocale->sSlug;
         $this->sLanguage            = $sLanguage;
-        $_sTransientPrefix          = AmazonAutoLinks_Registry::TRANSIENT_PREFIX;
-        $this->sTransientKey        = "_transient_{$_sTransientPrefix}_cookies_{$this->oLocale->sSlug}";
-        $this->sTransientKeyTimeout = "_transient_timeout_{$_sTransientPrefix}_cookies_{$this->oLocale->sSlug}";
     }
 
     /**
@@ -82,25 +67,17 @@ class AmazonAutoLinks_Locale_AmazonCookies extends AmazonAutoLinks_PluginUtility
      * @remark  Be aware that this method takes time, meaning slow as this performs at least two HTTP requests if not cached.
      */
     public function get() {
-
-        // Check cache.
-        $_aCachedCookies = $this->___getCookieTransient();
-        if ( ! empty( $_aCachedCookies ) ) {
-            return $_aCachedCookies;
-        }
-
-        $_aCookies = $this->___getCookies();
-        // It seems Amazon servers parses cookies from last.
-        // This is important when there are cookies with the same name.
-        $_aCookies = array_reverse( $_aCookies );
-        $this->___setCookieTransient( $_aCookies );
-        return $_aCookies;
-
+        return $this->___getCookies();
     }
         /**
          * @return WP_Http_Cookie[]
          */
         private function ___getCookies() {
+
+            $_aCustomCookies  = apply_filters( 'aal_filter_custom_amazon_cookies', array(), $this->oLocale, $this->sLanguage );
+            if ( ! empty( $_aCustomCookies ) ) {
+                return $_aCustomCookies;
+            }
 
             $_sURL            = $this->oLocale->getAssociatesURL();
             $_aRequestCookies = $this->___getAssociatesRequestCookiesGenerated( $_sURL, $this->sLocale, $this->sLanguage );
@@ -122,36 +99,9 @@ class AmazonAutoLinks_Locale_AmazonCookies extends AmazonAutoLinks_PluginUtility
 
             }
 
-            // The method does not exists any more.
-            return $_aLastCookies;
+            // It seems Amazon servers parses cookies from last. This is important when there are cookies with the same name.
+            return array_reverse( $_aLastCookies );
 
-        }
-
-        /**
-         * @return WP_Http_Cookie[]
-         * @since  4.3.4
-         */
-        private function ___getCookieTransient() {
-            if ( $this->isExpired( ( integer ) get_option( $this->sTransientKeyTimeout, 0 ) ) ) {
-                return array();
-            }
-            return $this->getAsArray( get_option( $this->sTransientKey, array() ) );
-        }
-        /**
-         * Sets an option looking like a transient in the options table.
-         * The data is stored as an option but with the transient name.
-         * This is to enable the autoload option but with an expiration time.
-         * By using set_transient(), if an expiration time is given, the autoload option will be disabled.
-         * @param  array   $aCookies
-         * @return boolean
-         * @since  4.3.4
-         */
-        private function ___setCookieTransient( array $aCookies ) {
-            $_sTransientPrefix = AmazonAutoLinks_Registry::TRANSIENT_PREFIX;
-            $_sNameTimeout     = "_transient_timeout_{$_sTransientPrefix}_cookies_{$this->oLocale->sSlug}";
-            $_sName            = "_transient_{$_sTransientPrefix}_cookies_{$this->oLocale->sSlug}";
-            update_option( $_sNameTimeout, time() + $this->iCacheDuration );
-            return update_option( $_sName, $aCookies );
         }
 
         /**

include/core/class-map.php
@@ -1,6 +1,6 @@
 <?php 
 /**
-    Amazon Auto Links v4.4.4b02 by miunosoft (Michael Uno) 
+    Amazon Auto Links v4.4.4 by miunosoft (Michael Uno) 
     Generates links of Amazon products just coming out today. You just pick categories and they appear even in JavaScript disabled browsers.
     <http://en.michaeluno.jp/amazon-auto-links>
     Copyright (c) 2013-2020, Michael Uno; Licensed under GPL v2 or later
@@ -97,6 +97,14 @@ return array(
     "AmazonAutoLinks_Proxy_Fetch_https" => AmazonAutoLinks_Registry::$sDirPath . "/include/core/component/proxy/utility/fetch/AmazonAutoLinks_Proxy_Fetch_https.php", 
     "AmazonAutoLinks_Proxy_Fetch_socks4" => AmazonAutoLinks_Registry::$sDirPath . "/include/core/component/proxy/utility/fetch/AmazonAutoLinks_Proxy_Fetch_socks4.php", 
     "AmazonAutoLinks_Proxy_Fetch_socks5" => AmazonAutoLinks_Registry::$sDirPath . "/include/core/component/proxy/utility/fetch/AmazonAutoLinks_Proxy_Fetch_socks5.php", 
+    "AmazonAutoLinks_Proxy_WebPageDumper_Loader" => AmazonAutoLinks_Registry::$sDirPath . "/include/core/component/proxy/web_page_dumper/AmazonAutoLinks_Proxy_WebPageDumper_Loader.php", 
+    "AmazonAutoLinks_Proxy_WebPageDumper_Admin" => AmazonAutoLinks_Registry::$sDirPath . "/include/core/component/proxy/web_page_dumper/admin/AmazonAutoLinks_Proxy_WebPageDumper_Admin.php", 
+    "AmazonAutoLinks_Proxy_WebPageDumper_Admin_Section" => AmazonAutoLinks_Registry::$sDirPath . "/include/core/component/proxy/web_page_dumper/admin/AmazonAutoLinks_Proxy_WebPageDumper_Admin_Section.php", 
+    "AmazonAutoLinks_Proxy_WebPageDumper_Admin_Tab_Help" => AmazonAutoLinks_Registry::$sDirPath . "/include/core/component/proxy/web_page_dumper/admin/AmazonAutoLinks_Proxy_WebPageDumper_Admin_Tab_Help.php", 
+    "AmazonAutoLinks_Proxy_WebPageDumper_Event_Ajax_TestAvailability" => AmazonAutoLinks_Registry::$sDirPath . "/include/core/component/proxy/web_page_dumper/event/ajax/AmazonAutoLinks_Proxy_WebPageDumper_Event_Ajax_TestAvailability.php", 
+    "AmazonAutoLinks_Proxy_WebPageDumper_Event_Filter_AmazonCookies" => AmazonAutoLinks_Registry::$sDirPath . "/include/core/component/proxy/web_page_dumper/event/filter/AmazonAutoLinks_Proxy_WebPageDumper_Event_Filter_AmazonCookies.php", 
+    "AmazonAutoLinks_Proxy_WebPageDumper_HTTPClient" => AmazonAutoLinks_Registry::$sDirPath . "/include/core/component/proxy/web_page_dumper/utility/AmazonAutoLinks_Proxy_WebPageDumper_HTTPClient.php", 
+    "AmazonAutoLinks_Proxy_WebPageDumper_Utility" => AmazonAutoLinks_Registry::$sDirPath . "/include/core/component/proxy/web_page_dumper/utility/AmazonAutoLinks_Proxy_WebPageDumper_Utility.php", 
     "AmazonAutoLinks_Shortcode" => AmazonAutoLinks_Registry::$sDirPath . "/include/core/component/shortcode/AmazonAutoLinks_Shortcode.php", 
     "AmazonAutoLinks_TemplateLoader" => AmazonAutoLinks_Registry::$sDirPath . "/include/core/component/template/AmazonAutoLinks_TemplateLoader.php", 
     "AmazonAutoLinks_TemplateResourceLoader" => AmazonAutoLinks_Registry::$sDirPath . "/include/core/component/template/AmazonAutoLinks_TemplateResourceLoader.php", 
@@ -446,7 +454,9 @@ return array(
     "AmazonAutoLinks_Event___Action_DeleteExpiredCaches" => AmazonAutoLinks_Registry::$sDirPath . "/include/core/main/event/action/AmazonAutoLinks_Event___Action_DeleteExpiredCaches.php", 
     "AmazonAutoLinks_Event___Action_HTTPCacheRenewal" => AmazonAutoLinks_Registry::$sDirPath . "/include/core/main/event/action/AmazonAutoLinks_Event___Action_HTTPCacheRenewal.php", 
     "AmazonAutoLinks_Event___Action_SimplePie_CacheRenewal" => AmazonAutoLinks_Registry::$sDirPath . "/include/core/main/event/action/AmazonAutoLinks_Event___Action_SimplePie_CacheRenewal.php", 
+    "AmazonAutoLinks_Main_Event_Action_AmazonCookiesRenewal" => AmazonAutoLinks_Registry::$sDirPath . "/include/core/main/event/action/AmazonAutoLinks_Main_Event_Action_AmazonCookiesRenewal.php", 
     "AmazonAutoLinks_Main_Event_Action_MoveDeprecatedTemporaryPluginSiteDirectory" => AmazonAutoLinks_Registry::$sDirPath . "/include/core/main/event/action/AmazonAutoLinks_Main_Event_Action_MoveDeprecatedTemporaryPluginSiteDirectory.php", 
+    "AmazonAutoLinks_Main_Event_Filter_AmazonCookies" => AmazonAutoLinks_Registry::$sDirPath . "/include/core/main/event/filter/AmazonAutoLinks_Main_Event_Filter_AmazonCookies.php", 
     "AmazonAutoLinks_Main_Event_Filter_Debug_UnknownUnitType" => AmazonAutoLinks_Registry::$sDirPath . "/include/core/main/event/filter/AmazonAutoLinks_Main_Event_Filter_Debug_UnknownUnitType.php", 
     "AmazonAutoLinks_Main_Event_Filter_HTTPClientArguments" => AmazonAutoLinks_Registry::$sDirPath . "/include/core/main/event/filter/AmazonAutoLinks_Main_Event_Filter_HTTPClientArguments.php", 
     "AmazonAutoLinks_Main_Event_Filter_HTTPClientArguments_AmazonCookies" => AmazonAutoLinks_Registry::$sDirPath . "/include/core/main/event/filter/AmazonAutoLinks_Main_Event_Filter_HTTPClientArguments_AmazonCookies.php", 

include/core/component/proxy/web_page_dumper/AmazonAutoLinks_Proxy_WebPageDumper_Loader.php
@@ -1,0 +1,44 @@
+<?php
+/**
+ * Amazon Auto Links
+ *
+ * Generates links of Amazon products just coming out today. You just pick categories and they appear even in JavaScript disabled browsers.
+ *
+ * http://en.michaeluno.jp/amazon-auto-links/
+ * Copyright (c) 2013-2020 Michael Uno
+ */
+
+/**
+ * Loads the Web Page Dumper sub-component.
+ *
+ * @package      Amazon Auto Links
+ * @since        4.5.0
+ */
+class AmazonAutoLinks_Proxy_WebPageDumper_Loader extends AmazonAutoLinks_PluginUtility {
+
+    static public $sDirPath;
+
+    public function __construct() {
+
+        self::$sDirPath  = dirname( __FILE__ );
+
+        if ( is_admin() ) {
+            new AmazonAutoLinks_Proxy_WebPageDumper_Admin;
+        }
+
+        $_oToolOption = AmazonAutoLinks_ToolOption::getInstance();
+        if ( ! $_oToolOption->get( array( 'web_page_dumper', 'enable' ), false ) ) {
+            return;
+        }
+
+        $this->___load();
+
+    }
+
+        private function ___load() {
+            new AmazonAutoLinks_Proxy_WebPageDumper_Event_Filter_AmazonCookies;
+            new AmazonAutoLinks_Proxy_WebPageDumper_Event_Ajax_TestAvailability;
+        }
+
+
+}

include/core/component/proxy/web_page_dumper/admin/AmazonAutoLinks_Proxy_WebPageDumper_Admin.php
@@ -1,0 +1,91 @@
+<?php
+/**
+ * Amazon Auto Links
+ *
+ * Generates links of Amazon products just coming out today. You just pick categories and they appear even in JavaScript disabled browsers.
+ *
+ * http://en.michaeluno.jp/amazon-auto-links/
+ * Copyright (c) 2013-2020 Michael Uno
+ */
+
+/**
+ * Loads the admin component of the Proxy/WebPageDumper component.
+ * 
+ * @since       4.5.0
+ */
+class AmazonAutoLinks_Proxy_WebPageDumper_Admin extends AmazonAutoLinks_PluginUtility {
+
+    /**
+     * @var string
+     */
+    public $sPageSlug;
+
+
+    /**
+     * Sets up properties and hooks.
+     */
+    public function __construct() {
+
+        $this->sPageSlug = AmazonAutoLinks_Registry::$aAdminPages[ 'tool' ];
+        add_action( 'load_' . $this->sPageSlug, array( $this, 'replyToLoadAdminPage' ) );
+        add_action( 'load_' . $this->sPageSlug . '_proxy', array( $this, 'replyToLoadTab' ) );
+
+    }
+
+    /**
+     *
+     * @param           AmazonAutoLinks_AdminPageFramework
+     * @callback        add_action()      load_{page slug}
+     */
+    public function replyToLoadAdminPage( $oFactory ) {
+
+        $oFactory->enqueueStyle( AmazonAutoLinks_Proxy_WebPageDumper_Loader::$sDirPath . '/asset/css/admin-web-page-dumper.css' , $this->sPageSlug );
+
+        // Tabs
+        new AmazonAutoLinks_Proxy_WebPageDumper_Admin_Tab_Help( $oFactory, $this->sPageSlug );
+
+    }
+
+    public function replyToLoadTab( $oFactory ) {
+
+        // Form Sections
+        new AmazonAutoLinks_Proxy_WebPageDumper_Admin_Section( $oFactory, $this->sPageSlug );
+
+        $this->___loadAjaxScript();
+
+    }
+
+
+        private function ___loadAjaxScript() {
+
+            $_sScriptHandle = 'aal_web_page_dumper_test_availability';
+            $_aScriptData   = array(
+                'ajaxURL'               => admin_url( 'admin-ajax.php' ),
+                'nonce'                 => wp_create_nonce( 'aal_nonce_ajax_' . $_sScriptHandle ),
+                'actionHookSuffix'      => $_sScriptHandle, // WordPress action hook name which follows after `wp_ajax_`
+                'spinnerURL'            => admin_url( 'images/loading.gif' ),
+                'label'                 => array(
+                    'enterURL'    => __( 'Please enter a URL.', 'amazon-auto-links' ),
+                    'testing'     => __( 'Testing...', 'amazon-auto-links' ),
+                    'alradyAdded' => __( 'Already added.', 'amazon-auto-links' ),
+                ),
+            );
+            wp_enqueue_script( 'jquery' );
+            wp_enqueue_script(
+                $_sScriptHandle,    // handle
+                $this->getSRCFromPath(
+                    $this->isDebugMode()
+                        ? AmazonAutoLinks_Proxy_WebPageDumper_Loader::$sDirPath . '/asset/js/web-page-dumper-tester.js'
+                        : AmazonAutoLinks_Proxy_WebPageDumper_Loader::$sDirPath . '/asset/js/web-page-dumper-tester.min.js'
+                ),
+                array( 'jquery' ),
+                true
+            );
+            wp_localize_script(
+                $_sScriptHandle,
+                'aalWebPageDumperTester',        // variable name on JavaScript side
+                $_aScriptData
+            );
+        }
+
+}

include/core/component/proxy/web_page_dumper/admin/AmazonAutoLinks_Proxy_WebPageDumper_Admin_Section.php
@@ -1,0 +1,134 @@
+<?php
+/**
+ * Amazon Auto Links
+ *
+ * Generates links of Amazon products just coming out today. You just pick categories and they appear even in JavaScript disabled browsers.
+ *
+ * http://en.michaeluno.jp/amazon-auto-links/
+ * Copyright (c) 2013-2020 Michael Uno
+ */
+
+/**
+ * Adds the 'Web Page Dumper' form section to the 'Proxies' tab.
+ * 
+ * @since       4.5.0
+ */
+class AmazonAutoLinks_Proxy_WebPageDumper_Admin_Section extends AmazonAutoLinks_AdminPage_Section_Base {
+
+    public $sTabSlug = 'proxy';
+
+    /**
+     * @return array
+     * @since  4.5.0
+     */
+    protected function _getArguments() {
+        return array(
+            'section_id'    => 'web_page_dumper',
+            'tab_slug'      => $this->sTabSlug,
+            'title'         => 'Web Page Dumper',
+            'description'   => array(
+                sprintf( __( '<a href="%1$s" target="_blank">%2$s</a> is a type of proxy server that assists HTTP requests.', 'amazon-auto-links' ), esc_url( 'https://github.com/michaeluno/web-page-dumper' ), 'Web Page Dumper' )
+            ),
+        );
+    }
+
+    /**
+     * @param AmazonAutoLinks_AdminPageFramework $oFactory
+     */
+    protected function _construct( $oFactory ) {
+    }
+
+    /**
+     * Adds form fields.
+     * @since       4.5.0
+     * @param       AmazonAutoLinks_AdminPageFramework $oFactory
+     * @param       string $sSectionID
+     * @return      void
+     */
+    protected function _addFields( $oFactory, $sSectionID ) {
+
+        $oFactory->addSettingFields(
+            $sSectionID, // the target section id
+            array(
+                'field_id'        => 'enable',
+                'title'           => __( 'Enable', 'amazon-auto-links' ),
+                'label'           => sprintf( __( 'Utilize %1$s to assist HTTP requests.', 'amazon-auto-links' ), 'Web Page Dumper' ),
+                'type'            => 'checkbox',
+            ),
+            array( 
+                'field_id'        => 'list',
+                'title'           => __( 'List', 'amazon-auto-links' ),
+                'type'            => 'textarea',
+                'attributes'      => array(
+                    'style'     => 'height: 200px; width: 100%;',
+                    // 'class'     => 'proxy-list',
+                ),
+                'class'           => array(
+                    'input' => 'list-web-page-dumper',
+                ),
+                'default'         => 'https://web-page-dumper.herokuapp.com/',
+                'description'     => array(
+                    sprintf( __( 'Enter addresses of %1$s one per line.', 'amazon-auto-links' ), 'Web Page Dumper' ),
+                    'e.g.<code>https://web-page-dumper.herokuapp.com/</code>',
+                    sprintf( __( 'It is better to create your own %1$s so that it will run faster.', 'amazon-auto-links' ), 'Web Page Dumper' ) . ' ' . sprintf( __( 'To create one, see <a href="%1$s">here</a>.', 'amazon-auto-links' ), esc_url( add_query_arg( array( 'tab' => 'web_page_dumper_help' ) ) ) ),
+                ),
+            ),
+            array(
+                'field_id'      => '_test',
+                'save'          => false,
+                'class'         => array(
+                    'fieldrow' => 'test-web-page-dumper'
+                ),
+                'content'       => array(
+                    array(
+                        'field_id'     => '_test_web_page_dumper',
+                        'save'         => false,
+                        'type'         => 'url',
+                        'before_input' => '<h4>' . __( 'Test', 'amazon-auto-links' ) . '</h4>',
+                        'after_input'  => "<a id='web-page-dumper-action-test' class='button button-secondary button-small button-action action-test-web-page-dumper'>" . __( 'Check', 'amazon-auto-links' ) . "</a>"
+                            . "<input id='web-page-dumper-action-add-to-list' disabled='disabled' type='button' class='button button-secondary button-small button-action action-add-to-list-web-page-dumper' value='" . esc_attr( __( 'Add to List', 'amazon-auto-links' ) ) . "'>",
+                        'attributes'   => array(
+                            'id'          => 'web-page-dumper-input-url',
+                            'name'        => '',
+                            'placeholder' => __( 'Type a URL of Web Page Dumper here to check if it is alive.', 'amazon-auto-links' ),
+                        ),
+                    ),
+                ),
+            ),
+            array()
+        );
+
+    }
+
+    public function validate( $aInputs, $aOldInputs, $oAdminPage, $aSubmitInfo ) {
+        if ( $aInputs[ 'enable' ] && ! $this->getElement( $aOldInputs, array( 'enable' ) ) ) {
+            $this->___renewAmazonCookies();
+        }
+        return $aInputs;
+    }
+        /**
+         * @since 4.5.0
+         */
+        private function ___renewAmazonCookies() {
+
+            $_sTransientPrefix     = AmazonAutoLinks_Registry::TRANSIENT_PREFIX;
+            foreach( AmazonAutoLinks_Locales::getLocales() as $_sLocale ) {
+
+                $_sTransientKey        = "_transient_{$_sTransientPrefix}_cookies_{$_sLocale}";
+                $_sTransientKeyTimeout = "_transient_timeout_{$_sTransientPrefix}_cookies_{$_sLocale}";
+                if ( ! $this->isExpired( get_option( $_sTransientKeyTimeout, 0 ) ) ) {
+                    $this->scheduleTask( 'aal_action_renew_amazon_cookies', array( $_sLocale, '' ) );
+                }
+
+                // Delete them anyway
+                delete_option( $_sTransientKey );
+                delete_option( $_sTransientKeyTimeout );
+
+                $_oVersatileCookies = new AmazonAutoLinks_VersatileFileManager_AmazonCookies( $_sLocale );
+                $_oVersatileCookies->delete();
+
+            }
+
+        }
+
+}

commit 593a3c62323347df9af71d6dfa0aa3c8e345a658

Fix: fix a bug that the cookie renewal event does not renew cookies
This started from d9b3a834 (several commits before).

include/core/_common/utility/locale/AmazonAutoLinks_Locale.php
@@ -159,12 +159,14 @@ class AmazonAutoLinks_Locale {
     }
 
     /**
-     * @param  string $sLanguage The preferred language.
-     * @return array  An array for the `cookies` argument of `wp_remote_request()`.
+     * @param  string  $sLanguage The preferred language.
+     * @param  boolean $bRenewCookies Whether to renew cookies.
+     * @return array   An array for the `cookies` argument of `wp_remote_request()`.
      * @since  4.3.4
+     * @since  4.5.0   Added the `$bRenewCookies` parameter.
      */
-    public function getHTTPRequestCookies( $sLanguage='' ) {
-        return $this->oLocale->getHTTPRequestCookies( $sLanguage );
+    public function getHTTPRequestCookies( $sLanguage='', $bRenewCookies=false ) {
+        return $this->oLocale->getHTTPRequestCookies( $sLanguage, $bRenewCookies );
     }
 
     /**

include/core/_common/utility/locale/AmazonAutoLinks_Locale_Base.php
@@ -200,17 +200,22 @@ abstract class AmazonAutoLinks_Locale_Base extends AmazonAutoLinks_PluginUtility
 
     /**
      * @see    WP_Http_Cookie
-     * @param  string $sLanguage The preferred language.
+     * @param  string  $sLanguage      The preferred language.
+     * @param  boolean $bRenewCookies  Whether to renew cookies. If this is true, the transient will not be used.
      * @return array|WP_Http_Cookie[]  An array for the `cookies` argument of `wp_remote_request()`.
      * @remark Be aware that this method takes time, meaning slow as this performs at least two HTTP requests if not cached.
      * @since  4.3.4
+     * @since  4.5.0    Added the `$bRenewCookies` parameter.
      */
-    public function getHTTPRequestCookies( $sLanguage='' ) {
+    public function getHTTPRequestCookies( $sLanguage='', $bRenewCookies=false ) {
 
-        $_aCachedCookies = $this->___getCookieTransient( $sLanguage );
-        if ( ! empty( $_aCachedCookies ) ) {
-            return $_aCachedCookies;
+        if ( ! $bRenewCookies ) {
+            $_aCachedCookies = $this->___getCookieTransient( $sLanguage );
+            if ( ! empty( $_aCachedCookies ) ) {
+                return $_aCachedCookies;
+            }
         }
+
         $_oCookieGetter = new AmazonAutoLinks_Locale_AmazonCookies( $this, $sLanguage );
         $_aCookies = $_oCookieGetter->get();
         $this->___setCookieTransient( $_aCookies, $_oCookieGetter );

include/core/main/event/action/AmazonAutoLinks_Main_Event_Action_AmazonCookiesRenewal.php
@@ -9,7 +9,9 @@
  */
 
 /**
- * Handles renewing Amazon site cookies.
+ * Handles renewing Amazon site cookies stores in a transient.
+ *
+ * This does not do anything to the cookie file stored in the system temporary area.
  *
  * @since        4.5.0
  */
@@ -28,10 +30,8 @@ class AmazonAutoLinks_Main_Event_Action_AmazonCookiesRenewal extends AmazonAutoL
      * @since  4.0.0
      */
     public function replyToRenewAmazonCookies( $sLocale, $sLanguage ) {
-        $_oLocaleSelector = new AmazonAutoLinks_Locale( $sLocale );
-        $_oLocale         = $_oLocaleSelector->get();
-        $_oCookieGetter   = new AmazonAutoLinks_Locale_AmazonCookies( $_oLocale, $sLanguage );
-        $_oCookieGetter->get();
+        $_oLocale  = new AmazonAutoLinks_Locale( $sLocale );
+        $_oLocale->getHTTPRequestCookies( $sLanguage, true );
     }
 
 }