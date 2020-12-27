bc5893f3718c4238aa2fb9128af9e527a309034d
Michael Uno <michael2@michaeluno.jp>
2020-12-28 05:26:06 +0900

    Fix: Fix a bug that embedded outputs were not possible with the locales of Sweden, Saudi Arabia, Singapole, UAE, Turkey, and China [4.4.6]

--- include/core/component/custom_oembed/oembed/AmazonAutoLinks_CustomOEmbed_Provider.php
+++ include/core/component/custom_oembed/oembed/AmazonAutoLinks_CustomOEmbed_Provider.php
@@ -45,6 +45,9 @@ class AmazonAutoLinks_CustomOEmbed_Provider {
             ),
             site_url()
         );
+        $aProviders = array(
+            "#https?://([a-z0-9-]+\.)?amazon\.[a-z]+(\.[a-z])?/.*#i" => array( $_sEndpointURL, true ),
+        ) + $aProviders; // with array union, the element will be placed at the top of the array.
         $aProviders[ "#https?://([a-z0-9-]+\.)?amazon\.(com|com\.mx|com\.br|ca)/.*#i" ] = array( $_sEndpointURL, true );
         $aProviders[ "#https?://([a-z0-9-]+\.)?amazon\.(co\.uk|de|fr|it|es|in|nl|ru)/.*#i" ] = array( $_sEndpointURL, true );
         $aProviders[ "#https?://([a-z0-9-]+\.)?amazon\.(co\.jp|com\.au)/.*#i" ] = array( $_sEndpointURL, true );
