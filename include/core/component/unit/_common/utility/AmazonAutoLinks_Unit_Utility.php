87bb7c5857bb3b082c6c76da6ab4670dcbb38d25
Michael Uno <michael2@michaeluno.jp>
2021-06-17 09:04:00 +0900

    Optimize: Use the display="none" attribute for invisible SVG

--- include/core/component/unit/_common/utility/AmazonAutoLinks_Unit_Utility.php
+++ include/core/component/unit/_common/utility/AmazonAutoLinks_Unit_Utility.php
@@ -805,10 +805,9 @@ class AmazonAutoLinks_Unit_Utility extends AmazonAutoLinks_PluginUtility {
             static $_aDefinitions   = array();
             $_sSVGDefinition        = '';
             $_sDefsGradient          = '';
-            $_sCSSInvisible         = 'position:fixed;height:0;width:0;bottom:0;left:0;z-index:-999;display:inline;opacity:0;';
 
             if ( ! isset( $_aDefinitions[ 'main' ] ) ) {
-                $_sSVGDefinition = "<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' width=0 height=0 viewBox='0 0 160 32' style='{$_sCSSInvisible}'>"
+                $_sSVGDefinition = "<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' width=0 height=0 viewBox='0 0 160 32' display='none'>"
                     . "<g id='amazon-rating-stars'>"
                         . "<path stroke='#E17B21' stroke-width='2' d='M 16.025391 0.58203125 L 11.546875 10.900391 L 0 12.099609 L 8.8222656 19.849609 L 6.1269531 31.382812 L 16.021484 25.294922 L 25.914062 31.382812 L 23.265625 19.849609 L 32 12.099609 L 20.388672 10.900391 L 16.025391 0.58203125 z M 32 12.099609 L 40.822266 19.849609 L 38.126953 31.382812 L 48.021484 25.294922 L 57.914062 31.382812 L 55.265625 19.849609 L 64 12.099609 L 52.388672 10.900391 L 48.025391 0.58203125 L 43.546875 10.900391 L 32 12.099609 z M 64 12.099609 L 72.822266 19.849609 L 70.126953 31.382812 L 80.021484 25.294922 L 89.914062 31.382812 L 87.265625 19.849609 L 96 12.099609 L 84.388672 10.900391 L 80.025391 0.58203125 L 75.546875 10.900391 L 64 12.099609 z M 96 12.099609 L 104.82227 19.849609 L 102.12695 31.382812 L 112.02148 25.294922 L 121.91406 31.382812 L 119.26562 19.849609 L 128 12.099609 L 116.38867 10.900391 L 112.02539 0.58203125 L 107.54688 10.900391 L 96 12.099609 z M 128 12.099609 L 136.82227 19.849609 L 134.12695 31.382812 L 144.02148 25.294922 L 153.91406 31.382812 L 151.26562 19.849609 L 160 12.099609 L 148.38867 10.900391 L 144.02539 0.58203125 L 139.54688 10.900391 L 128 12.099609 z' />"    // the actual vector images of 5 stars
                     . "</g>"
