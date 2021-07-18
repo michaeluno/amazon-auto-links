bb666e7fbf9154c1d2f9f20e0ed74e318a9f7078
Michael Uno <michael2@michaeluno.jp>
2021-07-19 00:24:57 +0900

    Optimize: Deprecate an unused parameter
    The `$bForce` parameter is not used so remove it.

--- include/core/component/template/option/AmazonAutoLinks_TemplateActivator.php
+++ include/core/component/template/option/AmazonAutoLinks_TemplateActivator.php
@@ -17,71 +17,65 @@ class AmazonAutoLinks_TemplateActivator extends AmazonAutoLinks_PluginUtility {
 
     public function __construct() {
 
-        add_action( 'aal_action_activate_templates', array( $this, 'replyToActivateTemplates' ), 10, 2 );;
-        add_action( 'aal_action_deactivate_templates', array( $this, 'replyToDeactivateTemplates' ), 10, 2 );;
+        add_action( 'aal_action_activate_templates', array( $this, 'replyToActivateTemplates' ), 10 );
+        add_action( 'aal_action_deactivate_templates', array( $this, 'replyToDeactivateTemplates' ), 10 );
 
     }
 
     /**
      * @param array an array holding template IDs or template option arrays.
-     * @param   boolean $bForce     Whether or not to override the existing `is_active` option.
      * This is sed for an addon to register new template with activated. The template list table's actions use this as true. true: yes, false: no.
      */
-    public function replyToActivateTemplates( array $aTemplateIDs, $bForce ) {
-        $this->___getTemplatesStatusToggled( $aTemplateIDs, true, $bForce );
+    public function replyToActivateTemplates( array $aTemplateIDs ) {
+        $this->___getTemplatesStatusToggled( $aTemplateIDs, true );
     }
     /**
      * @param array an array holding template IDs or template option arrays.
      */
-    public function replyToDeactivateTemplates( array $aTemplateIDs, $bForce ) {
-        $this->___getTemplatesStatusToggled( $aTemplateIDs, false, $bForce );
+    public function replyToDeactivateTemplates( array $aTemplateIDs ) {
+        $this->___getTemplatesStatusToggled( $aTemplateIDs, false );
     }
 
         /**
          * @param array $aTemplateIDs
          * @param boolean $bActivate true for activation; false for deactivation
-         * @param $bForce
          */
-        private function ___getTemplatesStatusToggled( array $aTemplateIDs, $bActivate, $bForce ) {
+        private function ___getTemplatesStatusToggled( array $aTemplateIDs, $bActivate ) {
             $_oTemplateOption = AmazonAutoLinks_TemplateOption::getInstance();
             $_aTemplates      = $this->getAsArray( $_oTemplateOption->get() );
             foreach( $aTemplateIDs as $_sID ) {
-                $_aTemplates = $this->___getTemplateStatusToggled( $_sID, $_aTemplates, $bActivate, $bForce );
+                $_aTemplates = $this->___getTemplateStatusToggled( $_sID, $_aTemplates, $bActivate );
             }
             $_oTemplateOption->aOptions = $_aTemplates;
             $_oTemplateOption->save();
 
         }
             /**
-             * @param $sID
-             * @param array $aTemplates
-             * @param $bActivate
-             * @param $bForce
-             *
+             * @param  string   $sID
+             * @param  array    $aTemplates
+             * @param  boolean  $bActivate
              * @return array
              */
-            private function ___getTemplateStatusToggled( $sID, array $aTemplates, $bActivate, $bForce ) {
+            private function ___getTemplateStatusToggled( $sID, array $aTemplates, $bActivate ) {
 
                 $_oTemplateOption   = AmazonAutoLinks_TemplateOption::getInstance();
                 $_aTemplate         = array();
 
                 if ( isset( $aTemplates[ $sID ] )  ) {
-                    if ( $bForce ) {
-                        // @since 3.10.0 If activating, renew the template information.
-                        if ( $bActivate ) {
-                            $_sDirPath  = $this->getAbsolutePathFromRelative( $sID );
-                            $_aTemplate = file_exists( $_sDirPath )
-                                ? $_oTemplateOption->getTemplateArrayByDirPath( $_sDirPath )
-                                : array();
-                            $_aTemplate = $this->getAsArray( $_aTemplate );
-                        }
-
-                        $aTemplates[ $sID ] = array(
-                            'is_active' => $bActivate,
-                        )
-                            + $_aTemplate
-                            + $this->getElementAsArray( $aTemplates, $sID );
+                    // @since 3.10.0 If activating, renew the template information.
+                    if ( $bActivate ) {
+                        $_sDirPath  = $this->getAbsolutePathFromRelative( $sID );
+                        $_aTemplate = file_exists( $_sDirPath )
+                            ? $_oTemplateOption->getTemplateArrayByDirPath( $_sDirPath )
+                            : array();
+                        $_aTemplate = $this->getAsArray( $_aTemplate );
                     }
+
+                    $aTemplates[ $sID ] = array(
+                        'is_active' => $bActivate,
+                    )
+                        + $_aTemplate
+                        + $this->getElementAsArray( $aTemplates, $sID );
                     return $aTemplates;
                 }
 
