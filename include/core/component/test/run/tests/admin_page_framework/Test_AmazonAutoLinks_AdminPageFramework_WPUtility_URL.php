ee7214406f78062055d7bd983a7886cf23427a3f
Michael Uno <michael2@michaeluno.jp>
2021-08-03 02:11:16 +0900

    Test: Add a test for the `AmazonAutoLinks_AdminPageFramework_WPUtility_URL` class.

---
+++ include/core/component/test/run/tests/admin_page_framework/Test_AmazonAutoLinks_AdminPageFramework_WPUtility_URL.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Amazon Auto Links
+ *
+ * Generates links of Amazon products just coming out today. You just pick categories and they appear even in JavaScript disabled browsers.
+ *
+ * https://en.michaeluno.jp/amazon-auto-links/
+ * Copyright (c) 2013-2021 Michael Uno
+ */
+
+/**
+ * Tests AmazonAutoLinks_AdminPageFramework_WPUtility_URL methods.
+ *  
+ * @package Amazon Auto Links
+ * @see     AmazonAutoLinks_AdminPageFramework_WPUtility_URL
+ * @since   4.7.0
+*/
+class Test_AmazonAutoLinks_AdminPageFramework_WPUtility_URL extends AmazonAutoLinks_UnitTest_Base {
+
+    /**
+     * @tags url
+     */
+    public function test_url() {
+        $this->_assertTrue( plugins_url( basename( __FILE__ ), __FILE__ ) === $this->getSRCFromPath( __FILE__ ) );
+    }
+
+}
