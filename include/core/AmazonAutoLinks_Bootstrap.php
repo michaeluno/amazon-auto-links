452b80094595c22def8e0dbdd8b6d03eb0b44c71
Michael Uno <michael2@michaeluno.jp>
2021-08-10 16:08:26 +0900

    Fix: Fix a raw HTTP request value set in a global variable

--- include/core/AmazonAutoLinks_Bootstrap.php
+++ include/core/AmazonAutoLinks_Bootstrap.php
@@ -90,12 +90,12 @@ final class AmazonAutoLinks_Bootstrap extends AmazonAutoLinks_AdminPageFramework
      * Sets up global variables.
      */
     public function setGlobals() {
-        
+
         if ( $this->bIsAdmin ) { 
         
             // The form transient key will be sent via the both get and post methods.
             $GLOBALS[ 'aal_transient_id' ] = isset( $_REQUEST[ 'transient_id' ] )
-                ? $_REQUEST[ 'transient_id' ]
+                ? _sanitize_text_fields( $_REQUEST[ 'transient_id' ] )
                 : AmazonAutoLinks_Registry::TRANSIENT_PREFIX 
                     . '_Form' 
                     . '_' . get_current_user_id() 
