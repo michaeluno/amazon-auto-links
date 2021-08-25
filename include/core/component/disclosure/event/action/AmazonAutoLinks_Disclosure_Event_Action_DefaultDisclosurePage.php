630662f578f9382c1a39221df6157af2bb9027b7
Michael Uno <michael2@michaeluno.jp>
2021-08-25 14:07:51 +0900

    Fix: Fix an issue that disclosure page creating is not done when the plugin is overwritten
    Via the WordPress UI through Add New -> Upload Plugin.

--- include/core/component/disclosure/event/action/AmazonAutoLinks_Disclosure_Event_Action_DefaultDisclosurePage.php
+++ include/core/component/disclosure/event/action/AmazonAutoLinks_Disclosure_Event_Action_DefaultDisclosurePage.php
@@ -21,6 +21,44 @@ class AmazonAutoLinks_Disclosure_Event_Action_DefaultDisclosurePage extends Amaz
     public function __construct() {
         add_action( 'aal_action_plugin_activated', array( $this, 'replyToDoOnPluginActivation' ) );
         add_action( 'upgrader_process_complete', array( $this, 'replyToDoOnPluginUpdate' ), 10, 2 );
+        add_action( 'upgrader_overwrote_package', array( $this, 'replyToDoOnPluginOverwrite' ), 10, 3 );
+    }
+
+    /**
+     * @param string $sPackagePath
+     * @param array  $aPluginInfo  Takes the following strucure.
+     * ```
+     * array(
+     *    [Name] => (string, length: 47) Amazon Auto Links - Drop Products without Price
+     *    [PluginURI] => (string, length: 42) https://en.michaeluno.jp/amazon-auto-links
+     *    [Version] => (string, length: 5) 1.0.0
+     *    [Description] => (string, length: 30) Drops products without a price
+     *    [Author] => (string, length: 23) Michael Uno (miunosoft)
+     *    [AuthorURI] => (string, length: 21) https://michaeluno.jp
+     *    [TextDomain] => (string, length: 0)
+     *    [DomainPath] => (string, length: 0)
+     *    [Network] => (boolean) false
+     *    [RequiresWP] => (string, length: 0)
+     *    [RequiresPHP] => (string, length: 0)
+     *    [UpdateURI] => (string, length: 0)
+     *    [Title] => (string, length: 47) Amazon Auto Links - Drop Products without Price
+     *    [AuthorName] => (string, length: 23) Michael Uno (miunosoft)
+     * )
+     * ```
+     * @param string $sContext
+     * @since 4.7.0
+     * @see   wp-admin/includes/class-plugin-upgrader.php
+     * @see   Plugin_Upgrader::install()
+     */
+    public function replyToDoOnPluginOverwrite( $sPackagePath, $aPluginInfo, $sContext ) {
+        if ( 'plugin' !== $sContext ) {
+            return;
+        }
+        $_aOurPlugin = get_plugin_data( AmazonAutoLinks_Registry::$sFilePath, false, false );
+        if ( $this->getElement( $_aOurPlugin, 'Name' ) !== $this->getElement( $aPluginInfo, 'Name' ) ) {
+            return;
+        }
+        $this->___doTheTask();
     }
 
     /**
@@ -47,9 +85,6 @@ class AmazonAutoLinks_Disclosure_Event_Action_DefaultDisclosurePage extends Amaz
         if ( ! $_bDetected ) {
             return;
         }
-        if ( $this->hasBeenCalled( get_class( $this ) ) ) {
-            return;
-        }
         $this->___doTheTask();
     }
 
@@ -57,15 +92,15 @@ class AmazonAutoLinks_Disclosure_Event_Action_DefaultDisclosurePage extends Amaz
      * @since 4.7.0
      */
     public function replyToDoOnPluginActivation() {
-        if ( $this->hasBeenCalled( get_class( $this ) ) ) {
-            return;
-        }
         $this->___doTheTask();
     }
         /**
          *
          */
         private function ___doTheTask() {
+            if ( $this->hasBeenCalled( get_class( $this ) ) ) {
+                return;
+            }
             if ( $this->___hasDisclosurePage() ) {
                 return;
             }
