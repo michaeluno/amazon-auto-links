e91899723647eed0ea7f05457b739c1bf87a2fb3
Michael Uno <michael2@michaeluno.jp>
2021-09-07 06:57:31 +0900

    Feature: Show debug outputs in the `Tools` page at the bottom
    When the `backend` component option is checked in the `Debug` setting field.

--- include/core/main/admin/tool/AmazonAutoLinks_ToolAdminPage_Tool.php
+++ include/core/main/admin/tool/AmazonAutoLinks_ToolAdminPage_Tool.php
@@ -24,6 +24,14 @@ class AmazonAutoLinks_ToolAdminPage_Tool extends AmazonAutoLinks_AdminPage_Page_
             'page_slug' => AmazonAutoLinks_Registry::$aAdminPages[ 'tool' ],
             'title'     => __( 'Tools', 'amazon-auto-links' ),
             'order'     => 900,
+            // 'style'     => AmazonAutoLinks_Main_Loader::$sDirPath . '/asset/css/admin.css',  // not sure why but the debug table seems to be fine without this
+            'script'    => array(
+                array(
+                    'src'           => AmazonAutoLinks_Main_Loader::$sDirPath . '/asset/js/accordion.js',
+                    'dependencies'  => array( 'jquery', 'jquery-ui-accordion', ),
+                    'in_footer'     => true,
+                ),
+            ),
         );
     }
 
@@ -45,6 +53,35 @@ class AmazonAutoLinks_ToolAdminPage_Tool extends AmazonAutoLinks_AdminPage_Page_
             $oFactory->setInPageTabTag( 'h2' );                
             $oFactory->enqueueStyle( AmazonAutoLinks_Main_Loader::$sDirPath . '/asset/css/admin.css' );
   
-        }    
+        }
+
+    /**
+     * @param AmazonAutoLinks_AdminPageFramework $oFactory
+     * @since 4.7.5
+     */
+    protected function _doPage( $oFactory ) {
+        $_oOption = AmazonAutoLinks_Option::getInstance();
+        if ( ! $_oOption->isDebug( 'back_end' ) ) {
+            return;
+        }
+        $_oToolOption = AmazonAutoLinks_ToolOption::getInstance();
+        echo "<div class='aal-accordion'>"
+            . "<h4>Debug - Tool Options</h4>"
+                . "<div>"
+                    . $this->getTableOfArray(
+                        $_oToolOption->get(),
+                        array(
+                            'table' => array(
+                                'class' => 'widefat striped fixed',
+                            ),
+                            'td'    => array(
+                                array( 'class' => 'width-one-fifth', ),  // first td
+                            )
+                        ),
+                    )
+                . "</div>"
+            . "</div>";
+
+    }
         
 }
