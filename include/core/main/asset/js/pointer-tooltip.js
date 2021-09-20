5111adb856b698c1b33af9d7acdf9e6705642a8f
Michael Uno <michael2@michaeluno.jp>
2021-09-21 01:39:44 +0900

    Style: Fix indents

--- include/core/main/asset/js/pointer-tooltip.js
+++ include/core/main/asset/js/pointer-tooltip.js
@@ -10,54 +10,54 @@
  * @description Extends the WordPress pointer tooltip widget to have additional features.
  * @version 1.0.0
  */
-(function($){
+(function ( $ ) {
 
-	var zindex = 9999;
+  var zindex = 9999;
 
-    // Extend wp.pointer jQuery widget
-    $.widget( 'aal.aalPointer', $.wp.pointer, {
-        /**
-         * Overrides the reposition() method.
-		 * The show() method is replaced with fadeIn().
-         */
-		reposition: function() {
-			var position;
+  // Extend wp.pointer jQuery widget
+  $.widget( 'aal.aalPointer', $.wp.pointer, {
+    /**
+     * Overrides the reposition() method.
+     * The show() method is replaced with fadeIn().
+     */
+    reposition: function () {
+      var position;
 
-			if ( this.options.disabled )
+      if ( this.options.disabled ) {
 				return;
+			}
 
 			position = this._processPosition( this.options.position );
 
-			// Reposition pointer.
-			this.pointer.css({
-				top: 0,
-				left: 0,
-				zIndex: zindex++ // Increment the z-index so that it shows above other opened pointers.
-			});
-			this.pointer.fadeIn( this.options.fadeIn );
-            var _optionsPosition = $.extend(
-                {
-                    of: this.element,
-                    collision: 'fit none'
-			    },
-                position
-            );
-			this.pointer.position( _optionsPosition ); // The object comes before this.options.position so the user can override position.of.
-			this.repoint();
-		},
-		_create: function() {
-       		this._super();
-       		if ( this.options.pointerHeight ) {
-				this.pointer
-					.css({
-						height: this.options.pointerHeight+'px'
-					});
-			}
-		},
-		get: function() {
-			return this.pointer;
-		}
+      // Reposition pointer.
+      this.pointer.css( {
+        top: 0,
+        left: 0,
+        zIndex: zindex++ // Increment the z-index so that it shows above other opened pointers.
+      } );
+      this.pointer.fadeIn( this.options.fadeIn );
+      var _optionsPosition = $.extend( {
+          of: this.element,
+          collision: 'fit none'
+        },
+        position
+      );
+      this.pointer.position( _optionsPosition ); // The object comes before this.options.position so the user can override position.of.
+      this.repoint();
+    },
+    _create: function () {
+      this._super();
+      if ( this.options.pointerHeight ) {
+        this.pointer
+          .css( {
+            height: this.options.pointerHeight + 'px'
+          } );
+      }
+    },
+    get: function () {
+      return this.pointer;
+    }
 
-    } );
+  } );
 
-}(jQuery));
+}( jQuery ));
--- template/_common/js/product-image-preview.js
+++ template/_common/js/product-image-preview.js
@@ -8,128 +8,122 @@
  * @name Image Preview Tooltip
  * @version 1.0.2
  */
-(function($){
+(function ( $ ) {
 
-    var imageSize = 420;
+  var imageSize = 420;
 
-    $( document ).ready( function() {
-        setMainThumbnailDataAttributes( $( 'body' ) );
-        initialize( '.amazon-auto-links-product-image img[data-large-src]' );
+  $( document ).ready( function () {
+    setMainThumbnailDataAttributes( $( 'body' ) );
+    initialize( '.amazon-auto-links-product-image img[data-large-src]' );
+  } );
+
+  $( 'body' ).on( 'aal_ajax_loaded_unit', function ( event ) {
+    setMainThumbnailDataAttributes( event.target );
+    initialize( $( event.target ).find( '.amazon-auto-links-product-image img[data-large-src]' ) );
+  } );
+
+  function setMainThumbnailDataAttributes( target ) {
+    $( target ).find( '.amazon-product-thumbnail-container[data-large-src]' ).each( function () {
+      $( this ).find( '.amazon-product-thumbnail img' ).attr( 'data-large-src', $( this ).attr( 'data-large-src' ) )
+        .attr( 'data-href', $( this ).attr( 'data-href' ) );
     } );
+  }
 
-    $( 'body' ).on( 'aal_ajax_loaded_unit', function( event ) {
-        setMainThumbnailDataAttributes( event.target );
-        initialize( $( event.target ).find( '.amazon-auto-links-product-image img[data-large-src]' ) );
+  function initialize( target ) {
+
+    var _imagePreviewPopup = $( target );
+    _imagePreviewPopup.on( 'click', function () {
+      return false; // disable click
     } );
+    _imagePreviewPopup.on( 'mouseover', function () {
 
-    function setMainThumbnailDataAttributes( target ) {
-        $( target ).find( '.amazon-product-thumbnail-container[data-large-src]' ).each( function(){
-            $( this ).find( '.amazon-product-thumbnail img' ).attr( 'data-large-src', $( this ).attr( 'data-large-src' ) )
-                .attr( 'data-href', $( this ).attr( 'data-href' ) );
-        } );
-    }
+      var _srcImg = $( this ).data( 'largeSrc' );
+      if ( ! _srcImg ) {
+        return;
+      }
 
-    // @deprecated
-    // function setImageData( target ) {
-    //     $( target ).each( function() {
-    //         $( this ).data( 'href', $( this ).closest( 'a' ).attr( 'href' ) );
-    //             $( this ).data( 'largeSrc', $( this ).attr( 'src' ).replace( /(?<=\.*[_A-Z,\.]+)(\d+)(?=[,_\.])/g, imageSize ) );
-    //     } );
-    // }
+      var _container = $( this ).closest( '.amazon-product-container, .amazon-auto-links' ),
+        // var _container = $( this ).closest( '.amazon-product-container' ),
+        _body = $( 'body' );
+      var _maxSize = Math.min( _body.width(), _body.height() );
 
-    function initialize( target ) {
+      _maxSize = _maxSize < imageSize
+        ? (_maxSize * 0.8)    // this occurs in the embedded view. This is to prevent the image to be cut off
+        : imageSize;
 
-        var _imagePreviewPopup = $( target );
-        _imagePreviewPopup.on( 'click', function() {
-            return false; // disable click
-        });
-        _imagePreviewPopup.on( 'mouseover', function() {
+      // Open the tooltip
+      $( this ).aalPointer( {
+        pointerClass: 'aal-image-preview-tooltip',
+        pointerWidth: parseInt( _maxSize ),
+        pointerHeight: parseInt( _maxSize ),
+        content: function () {
+          return "<a href='" + $( this ).data( 'href' ) + "' target='_blank'><img src='" + $( this ).data( 'largeSrc' ) + "' /></a>";
+        },
+        position: {
+          edge: 'bottom',
+          align: 'center',
+          within: _container,
+          collision: 'fit',   // don't exceed the container set with the `within` argument
+        },
+        fadeIn: 300,
+        buttons: function () {
+        },
+        close: function () {
+        },
+        show: function ( event, t ) {
+          t.pointer.fadeIn( 300 );
+          t.opened();
+        },
+        hide: function ( event, t ) {
+          t.pointer.fadeOut( 300 );
+          t.closed();
+        },
+      } );
 
-            var _srcImg = $( this ).data( 'largeSrc' );
-            if ( ! _srcImg ) {
-                return;
-            }
+      var _self = this;
 
-            var _container = $( this ).closest( '.amazon-product-container, .amazon-auto-links' ),
-            // var _container = $( this ).closest( '.amazon-product-container' ),
-                _body      = $( 'body' );
-            var _maxSize = Math.min( _body.width(), _body.height() );
+      // Wait for the image to load to check its dimension
+      var img = new Image();
+      img.onload = function () {
+        var _pointer = $( _self ).aalPointer( 'get' );
+        // Check portrait or landscape
+        // if landscape
+        if ( this.width > this.height ) {
+          _pointer.css( 'height', '' );
+        } else {
+          var _heightBody = _body.height();
+          if ( _heightBody < this.height + 24 ) { // padding top: 12px + padding bottom: 12px
+            _pointer.css( 'height', (_heightBody * 0.80) + 'px' );
+          }
+          _pointer.css( 'width', '' );
+        }
+        $( _self ).aalPointer( 'open' );
+      };
+      img.src = _srcImg;  // set the image
 
-            _maxSize = _maxSize < imageSize
-                ? ( _maxSize * 0.8 )    // this occurs in the embedded view. This is to prevent the image to be cut off
-                : imageSize;
-
-            // Open the tooltip
-            $( this ).aalPointer({
-                pointerClass: 'aal-image-preview-tooltip',
-                pointerWidth: parseInt( _maxSize ),
-                pointerHeight: parseInt( _maxSize ),
-                content: function() {
-                    return "<a href='" + $( this ).data( 'href' ) + "' target='_blank'><img src='" + $( this ).data( 'largeSrc' ) + "' /></a>";
-                },
-                position: {
-                    edge: 'bottom',
-                    align: 'center',
-                    within: _container,
-                    collision: 'fit',   // don't exceed the container set with the `within` argument
-                },
-                fadeIn: 300,
-                buttons: function() {},
-                close: function() {},
-                show: function( event, t ) {
-                    t.pointer.fadeIn( 300 );
-                    t.opened();
-                },
-                hide: function( event, t ) {
-                    t.pointer.fadeOut( 300 );
-                    t.closed();
-                },
-            });
-
-            var _self    = this;
-
-            // Wait for the image to load to check its dimension
-            var img = new Image();
-            img.onload = function() {
-                var _pointer = $( _self ).aalPointer( 'get' );
-                // Check portrait or landscape
-                // if landscape
-                if ( this.width > this.height ) {
-                    _pointer.css( 'height', '' );
-                } else {
-                    var _heightBody = _body.height();
-                    if ( _heightBody < this.height + 24 ) { // padding top: 12px + padding bottom: 12px
-                        _pointer.css( 'height', ( _heightBody * 0.80 ) + 'px' );
-                    }
-                    _pointer.css( 'width', '' );
-                }
-                $( _self ).aalPointer( 'open' );
-            };
-            img.src = _srcImg;  // set the image
-
-            // var _oPointer = $( this ).aalPointer( 'get' );
+      // var _oPointer = $( this ).aalPointer( 'get' );
 
 
-            // Handle toolitip closing
-            $( this ).add( '.aal-image-preview-tooltip' ).on( 'mouseleave', function(){
+      // Handle toolitip closing
+      $( this ).add( '.aal-image-preview-tooltip' ).on( 'mouseleave', function () {
 
-                var _selfMouseLeave = this;
-                // Set a timeout for the tooltip to close, allowing us to clear this trigger if the mouse comes back over
-                var _timeoutId = setTimeout(function(){
-                    $( _self ).aalPointer( 'close' );
-                    $( _self ).off( 'mouseleave' );
-                    $( _selfMouseLeave ).off( 'mouseleave' );
-                    $( _self ).off( 'mouseenter' );
-                    $( _selfMouseLeave ).off( 'mouseenter' );
-                }, 200 );
-                $( _self ).data( 'timeoutId', _timeoutId );
+        var _selfMouseLeave = this;
+        // Set a timeout for the tooltip to close, allowing us to clear this trigger if the mouse comes back over
+        var _timeoutId = setTimeout( function () {
+          $( _self ).aalPointer( 'close' );
+          $( _self ).off( 'mouseleave' );
+          $( _selfMouseLeave ).off( 'mouseleave' );
+          $( _self ).off( 'mouseenter' );
+          $( _selfMouseLeave ).off( 'mouseenter' );
+        }, 200 );
+        $( _self ).data( 'timeoutId', _timeoutId );
 
-            } );
-            $( this ).add( '.aal-image-preview-tooltip' ).on( 'mouseenter', function(){
-                clearTimeout( $( _self ).data('timeoutId' ) );
-            });
+      } );
+      $( this ).add( '.aal-image-preview-tooltip' ).on( 'mouseenter', function () {
+        clearTimeout( $( _self ).data( 'timeoutId' ) );
+      } );
 
-        });
-    }
+    } );
+  }
 
-}(jQuery));
+}( jQuery ));
