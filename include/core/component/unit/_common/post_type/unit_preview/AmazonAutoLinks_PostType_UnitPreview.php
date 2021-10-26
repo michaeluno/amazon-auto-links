10d49ad35f3474be5feabd1a4ae1e6ba847b1bac
Michael Uno <michael2@michaeluno.jp>
2021-10-26 17:25:38 +0900

    Style: Rename public callback method names

--- include/core/component/unit/_common/post_type/unit_preview/AmazonAutoLinks_PostType_UnitPreview.php
+++ include/core/component/unit/_common/post_type/unit_preview/AmazonAutoLinks_PostType_UnitPreview.php
@@ -11,25 +11,26 @@
 /**
  * Creates Auto Amazon Links custom preview post type.
  * 
- * @since       2.2.0
+ * @since 2.2.0
  */
 class AmazonAutoLinks_PostType_UnitPreview {
     
     /**
-     * 
-     * @since       2.2.0
+     * @var   string
+     * @since 2.2.0
      */ 
     protected $sDefaultPreviewSlug = '';
     
     /**
-     * @since       2.2.0
+     * @var   string
+     * @since 2.2.0
      */
     protected $sPreviewPostTypeSlug = '';
       
     /**
      * Sets up hooks and properties.
-     * 
-     * @since       2.2.0
+     *
+     * @since 2.2.0
      */
     public function __construct( $sPreviewPostTypeSlug='' ) {
         
@@ -38,15 +39,10 @@ class AmazonAutoLinks_PostType_UnitPreview {
         if ( ! $_oOption->isCustomPreviewPostTypeSet() ) {
             return;
         }
-        
+
         // Properties
         $this->sDefaultPreviewSlug  = AmazonAutoLinks_Registry::$aPostTypes[ 'unit' ];
-        $this->sPreviewPostTypeSlug = $sPreviewPostTypeSlug
-            ? $sPreviewPostTypeSlug
-            : $_oOption->get( 
-                'unit_preview', 
-                'preview_post_type_slug' 
-            );
+        $this->sPreviewPostTypeSlug = $sPreviewPostTypeSlug ? $sPreviewPostTypeSlug : $_oOption->get( 'unit_preview', 'preview_post_type_slug' );
         
         if ( ! $this->sPreviewPostTypeSlug ) {
             return;
@@ -54,27 +50,27 @@ class AmazonAutoLinks_PostType_UnitPreview {
         
         // Hooks
         /// Post Type
-        add_action( 'init', array( $this, '_replyToRegisterCustomPreviewPostType' ) );
-        
+        add_action( 'init', array( $this, 'replyToRegisterCustomPreviewPostType' ) );
+    
         /// Modify the links
-        add_filter( 'post_row_actions', array( $this, '_replyToAddViewActionLink' ), 10, 2 );
-        add_filter( 'post_type_link', array( $this, '_replyToModifyPermalinks' ), 10, 3 );
-        add_filter( 'post_link', array( $this, '_replyToModifyPermalinks' ), 10, 3 );
-        add_filter( "previous_post_link", array( $this, '_replyToModifyPostLink' ), 10, 4 );
-        add_filter( "next_post_link", array( $this, '_replyToModifyPostLink' ), 10, 4 );    
+        add_filter( 'post_row_actions', array( $this, 'replyToAddViewActionLink' ), 10, 2 );
+        add_filter( 'post_type_link', array( $this, 'replyToModifyPermalinks' ), 10, 3 );
+        add_filter( 'post_link', array( $this, 'replyToModifyPermalinks' ), 10, 3 );
+        add_filter( "previous_post_link", array( $this, 'replyToModifyPostLink' ), 10, 4 );
+        add_filter( "next_post_link", array( $this, 'replyToModifyPostLink' ), 10, 4 );    
 
         /// Modify database queries
-        add_filter( 'request', array( $this, '_replyToModifyDatabaseQuery' ) );
+        add_filter( 'request', array( $this, 'replyToModifyDatabaseQuery' ) );
 
     }
     
     /**
      * Modifies the action link of the post listing table.
      * 
-     * @callback    filter      post_row_actions
-     * @return      array       The action link definition array.
+     * @callback add_filter() post_row_actions
+     * @return   array        The action link definition array.
      */
-    public function _replyToAddViewActionLink( $aActions, $oPost ) {
+    public function replyToAddViewActionLink( $aActions, $oPost ) {
 
         if ( $this->sDefaultPreviewSlug !== $oPost->post_type ) {
             return $aActions;
@@ -90,15 +86,14 @@ class AmazonAutoLinks_PostType_UnitPreview {
     }
       
     /**
-     * @param       string      $output     The adjacent post link.
-     * @param       string      $format     Link anchor format.
-     * @param       string      $link       Link permalink format.
-     * @param       WP_Post     $post       The adjacent post.
-     * @callback    filter      previous_post_link
-     * @callback    filter      next_post_link
+     * @param    string       $sOutput    The adjacent post link.
+     * @param    string       $format     Link anchor format.
+     * @param    string       $link       Link permalink format.
+     * @param    WP_Post      $oPost      The adjacent post.
+     * @callback add_filter() previous_post_link
+     * @callback add_filter() next_post_link
      */
-    public function _replyToModifyPostLink( $sOutput, $format, $link, $oPost ) {
-        
+    public function replyToModifyPostLink( $sOutput, $format, $link, $oPost ) {
         if ( ! isset( $oPost->post_type ) ) {
             return $sOutput;
         }
@@ -106,7 +101,6 @@ class AmazonAutoLinks_PostType_UnitPreview {
             return $sOutput;
         }
         return $this->___getDefaultPostTypeSlugReplaced( $sOutput );
-
     }
 
     /**
@@ -115,11 +109,10 @@ class AmazonAutoLinks_PostType_UnitPreview {
      * If this is not created, the default plugin unit post type will be used.
      * And if this is created, the default one will be publicly disabled except some admin ui functionality.
      * 
-     * @callback        action      init
-     * @since           2.2.0
-     * @return          void
+     * @callback add_action() init
+     * @since    2.2.0
      */
-    public function _replyToRegisterCustomPreviewPostType() {
+    public function replyToRegisterCustomPreviewPostType() {
 
         if ( ! $this->sPreviewPostTypeSlug ) {
             return;
@@ -139,14 +132,14 @@ class AmazonAutoLinks_PostType_UnitPreview {
     }    
         /**
          * 
-         * @since       2.2.0
-         * @callback    filter      post_link
-         * @param       string      $sPermalink     The post's permalink.
-         * @param       WP_Post     $oPost          The post in question.
-         * @param       boolean     $bLeaveName     Whether to keep the post name.
-         * @return      string
+         * @since    2.2.0
+         * @callback add_filter() post_link
+         * @param    string       $sPermalink     The post's permalink.
+         * @param    WP_Post      $oPost          The post in question.
+         * @param    boolean      $bLeaveName     Whether to keep the post name.
+         * @return   string
          */
-        public function _replyToModifyPermalinks( $sPermalink, $oPost, $bLeaveName ) {
+        public function replyToModifyPermalinks( $sPermalink, $oPost, $bLeaveName ) {
 
             if ( $this->sDefaultPreviewSlug !== $oPost->post_type ) {
                 return $sPermalink;
@@ -184,7 +177,7 @@ class AmazonAutoLinks_PostType_UnitPreview {
      * @callback    filter      request
      * @return      array       The database query request array.
      */
-    public function _replyToModifyDatabaseQuery( $aRequest ) {
+    public function replyToModifyDatabaseQuery( $aRequest ) {
 
         $_oOption = AmazonAutoLinks_Option::getInstance();
     
