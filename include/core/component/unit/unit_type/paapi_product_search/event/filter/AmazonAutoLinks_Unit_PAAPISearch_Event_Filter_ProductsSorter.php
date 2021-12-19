52b928c38e89c049b3a31c617fe7479cb350adfb
Michael Uno <michael2@michaeluno.jp>
2021-12-19 11:43:12 +0900

    Optimize: Change the timing of shuffling products for the PA-API Product Search units
    Rather than shuffling after truncating items, shuffle before it so that the same items won't always be displayed.

--- include/core/component/unit/unit_type/paapi_product_search/event/filter/AmazonAutoLinks_Unit_PAAPISearch_Event_Filter_ProductsSorter.php
+++ include/core/component/unit/unit_type/paapi_product_search/event/filter/AmazonAutoLinks_Unit_PAAPISearch_Event_Filter_ProductsSorter.php
@@ -12,7 +12,7 @@
  *
  * @since 5.0.0
  */
-class AmazonAutoLinks_Unit_PAAPISearch_Event_Filter_ProductsSorter extends AmazonAutoLinks_Unit_UnitType_Common_Event_Filter_ProductsFormatter_Base {
+class AmazonAutoLinks_Unit_PAAPISearch_Event_Filter_ProductsSorter extends AmazonAutoLinks_Unit_UnitType_Common_Event_Filter_ProductsFetcher_Base {
 
     public $sUnitType = 'search';
 
@@ -23,7 +23,7 @@ class AmazonAutoLinks_Unit_PAAPISearch_Event_Filter_ProductsSorter extends Amazo
      * @return array
      * @since  5.0.0
      */
-    protected function _getItemsFormatted( $aProducts ) {
+    protected function _getItemsFromSource( $aProducts ) {
         if ( $this->oUnitOutput->oUnitOption->get( 'shuffle' ) ) {
             shuffle( $aProducts ); // [4.7.0+] For the Product Search units, as they don't have the 'random' sort order, this option makes it possible to shuffule products.
         }
