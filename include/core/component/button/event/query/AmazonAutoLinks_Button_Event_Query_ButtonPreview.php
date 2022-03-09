0dcd2541e695a4c38792b2bff8207126e77ced04
Michael Uno <michael2@michaeluno.jp>
2022-03-09 21:00:20 +0900

    Refactor: Divide a method into a few smaller methods

--- include/core/component/button/event/query/AmazonAutoLinks_Button_Event_Query_ButtonPreview.php
+++ include/core/component/button/event/query/AmazonAutoLinks_Button_Event_Query_ButtonPreview.php
@@ -8,11 +8,10 @@
  * Copyright (c) 2013-2021 Michael Uno
  */
 
-
 /**
  * Outputs the button preview.
  *
- * @since      4.3.0
+ * @since 4.3.0
  */
 class AmazonAutoLinks_Button_Event_Query_ButtonPreview extends AmazonAutoLinks_PluginUtility {
 
@@ -41,23 +40,26 @@ class AmazonAutoLinks_Button_Event_Query_ButtonPreview extends AmazonAutoLinks_P
         wp_enqueue_script( 'jquery' );
         $_sButtonLabel   = $this->getHTTPQueryGET( 'button-label', 'Buy Now' );
         $_iButtonID      = ( integer ) $this->getHTTPQueryGET( 'button-id', 0 );
-        $_sHeader        = $this->getOutputBuffer( 'wp_head' );
-        $_sHeader        = force_balance_tags( $_sHeader );
-        $_sHeader        = str_replace( array( "\n", "\r\n", "\r" ), '', $_sHeader ); // prevents `&#13;` from being inserted
         $_oDOM           = new AmazonAutoLinks_DOM;
-        $_oDoc           = $_oDOM->loadDOMFromHTML( "<html><head>" . $_sHeader . "</head><body></body></html>" );
-        $_oDOM->removeTags( $_oDoc, array( 'script' ) );    // in order to use jQuery, comment out this line.
+        $_oDoc           = $_oDOM->loadDOMFromHTML(
+            "<html><head>" . $this->___getHTMLHeaderConstructed() . "</head><body></body></html>"
+        );
+        $this->___removeScriptTags(
+            $_oDoc,
+            array() // to allow jQuery, pass `array( 'jquery' )` to the second parameter
+        );
         $_oXpath         = new DOMXPath( $_oDoc );
-        $_oTags          = $_oXpath->query( "/html/head" );
-        $_oTag           = $_oTags->item( 0 );
+        $_oHeadTags      = $_oXpath->query( "/html/head" );
+        $_oHeadTag       = $_oHeadTags->item( 0 );
         $_sMin           = $this->isDebugMode() ? '' : '.min';
         $_sStylesheetURL = $this->getSRCFromPath( AmazonAutoLinks_ButtonLoader::$sDirPath . "/asset/css/button-preview-framed-page{$_sMin}.css" );
         $_sScriptURL     = $this->getSRCFromPath( AmazonAutoLinks_ButtonLoader::$sDirPath . "/asset/js/button-preview-framed-page{$_sMin}.js" );
+
 ?>
 <!DOCTYPE html>
-<html <?php language_attributes(); ?> class="no-js no-svg">
+<html <?php language_attributes(); ?>>
     <?php echo "<head>"
-       . $_oDOM->getInnerHTML( $_oTag )
+       . $_oDOM->getInnerHTML( $_oHeadTag )
        . "<script id='button-preview-framed-js' src='" . esc_url( $_sScriptURL ) . "' type='text/javascript'></script>"
        . "<link rel='stylesheet' id='button-preview-framed-style' href='" . esc_url( $_sStylesheetURL ) . "' media='all'>"
     . "</head>"; ?>
@@ -70,5 +72,43 @@ class AmazonAutoLinks_Button_Event_Query_ButtonPreview extends AmazonAutoLinks_P
         <?php
         exit;
     }
+
+        /**
+         * @return string
+         */
+        private function ___getHTMLHeaderConstructed() {
+            $_sHeader = $this->getOutputBuffer( 'wp_head' );
+            $_sHeader = force_balance_tags( $_sHeader );
+            return str_replace( array( "\n", "\r\n", "\r" ), '', $_sHeader ); // prevents `&#13;` from being inserted
+        }
+        /**
+         * Removes specified tags from the given dom node.
+         * @param DOMDocument $oDom
+         * @param array       $aIDPrefixesToAllow
+         */
+        private function ___removeScriptTags( DOMDocument $oDom, $aIDPrefixesToAllow=array() ) {
+            $_oXpath    = new DOMXPath( $oDom );
+            $_oNodeList = $_oXpath->query( "//*/script" );
+            if ( false === $_oNodeList ) {
+                return;
+            }
+            foreach( $_oNodeList as $_oNode ) {
+                if ( $this->___isAllowedScript( $aIDPrefixesToAllow, $_oNode->getAttribute( 'id' ) ) ) {
+                    continue;
+                }
+                /**
+                 * @var DOMNode $_oNode
+                 */
+                $_oNode->parentNode->removeChild( $_oNode );
+            }
+        }
+            private function ___isAllowedScript( array $aIDPrefixes, $sAttribute ) {
+                foreach( $aIDPrefixes as $_sIDPrefix ) {
+                    if ( $this->hasPrefix( $_sIDPrefix, $sAttribute ) ) {
+                        return true;
+                    }                    
+                }
+                return false;
+            }
 
 }
