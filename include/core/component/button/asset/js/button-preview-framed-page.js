824cf45eca069d74eabd0a515d533b3bed96b7a2
Michael Uno <michael2@michaeluno.jp>
2022-03-14 20:18:31 +0900

    Optimize: Add a parameter to send a button ID
    This is required for an upcoming change that refines the button preview UI in the unit definition screen.

--- include/core/component/button/asset/js/button-preview-framed-page.js
+++ include/core/component/button/asset/js/button-preview-framed-page.js
@@ -1,12 +1,22 @@
+/**
+ * @name:    Button Preview Framed Page
+ * @version: 1.1.0
+ */
 window.addEventListener( 'DOMContentLoaded', function ( e ) {
-  var _oButton = document.getElementById( "preview-button" );
-  var _iWidth  = 0;
-  var _iHeight = 0;
+  setParentButtonPreviewIframeStyle();
+} );
+
+function setParentButtonPreviewIframeStyle() {
+  var _oButton   = document.getElementById( "preview-button" );
+  var _iWidth    = 0;
+  var _iHeight   = 0;
+  var _iButtonID = 0;
   if ( 'undefined' !== typeof _oButton && null !== _oButton ) {
     _iWidth    = _oButton.offsetWidth;
     _iHeight   = _oButton.offsetHeight;
+    _iButtonID = _oButton.getAttribute( 'data-button-id' );
   }
   if ( 'undefined' !== typeof parent.aalSetButtonPreviewIframeStyle ) {
-    window.parent.aalSetButtonPreviewIframeStyle( _iWidth, _iHeight );
+    window.parent.aalSetButtonPreviewIframeStyle( _iWidth, _iHeight, _iButtonID );
   }
-} );
+}
