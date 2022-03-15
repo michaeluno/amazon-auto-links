67e2109536f0735ea378ecdf45c34a5fb278c6f2
Michael Uno <michael2@michaeluno.jp>
2022-03-15 12:24:25 +0900

    Style: Clean code

--- include/core/component/button/post_type/AmazonAutoLinks_PostType_Button_ListTable.php
+++ include/core/component/button/post_type/AmazonAutoLinks_PostType_Button_ListTable.php
@@ -5,21 +5,21 @@
  * Generates links of Amazon products just coming out today. You just pick categories and they appear even in JavaScript disabled browsers.
  *
  * https://en.michaeluno.jp/amazon-auto-links/
- * Copyright (c) 2013-2021 Michael Uno
+ * Copyright (c) 2013-2022 Michael Uno
  */
 
 /**
- * Defines button post type.
+ * A base class for the button post type, overriding list table methods.
  * 
- * @since       3
+ * @since 3
  */
 abstract class AmazonAutoLinks_PostType_Button_ListTable extends AmazonAutoLinks_AdminPageFramework_PostType {
     
     /**
     * Defines the column header of the unit listing table.
     * 
-    * @callback     filter      columns_{post type slug}
-    * @return       array
+    * @callback add_filter() columns_{post type slug}
+    * @return   array
     */
     public function replyToModifyColumnHeader( $aHeaderColumns ) {    
         return array(
@@ -28,14 +28,12 @@ abstract class AmazonAutoLinks_PostType_Button_ListTable extends AmazonAutoLinks
             'status'                => __( 'Status', 'amazon-auto-links' ),
             'shortcode'             => __( 'Shortcode', 'amazon-auto-links' ),
             'preview'               => __( 'Preview', 'amazon-auto-links' ),
-            // 'template'              => __( 'Template', 'amazon-auto-links' ),
-            // 'code'                  => __( 'Shortcode / PHP Code', 'amazon-auto-links' ),
         );                      
     }
 
     /**
-     * @callback        filter      cell_{post type slug}_{column slug}
-     * @return string
+     * @callback add_filter() cell_{post type slug}_{column slug}
+     * @return   string
      */
     public function cell_aal_button_shortcode( $sCell, $iPostID ) {
         $_sShortcode = AmazonAutoLinks_Registry::$aShortcodes[ 'button' ];
@@ -44,19 +42,16 @@ abstract class AmazonAutoLinks_PostType_Button_ListTable extends AmazonAutoLinks
                 . "<span>"
                 . "[{$_sShortcode} id='{$iPostID}' asin='1118987241' label='{$_sLabel}']"
                . "</span>"
-//               . "<span class='description'> <i>* " . __( 'Change the <code>asin</code> parameter value to your desired product ASIN.', 'amazon-auto-links' ) . "</i></p>"
             . "</p>";
     }
 
     /**
-     * @return string
-     * @callback        filter      cell_{post type slug}_{column slug}
+     * @callback add_filter() cell_{post type slug}_{column slug}
+     * @return   string
      */
     public function cell_aal_button_preview( $sCell, $iPostID ) {
         return $sCell 
-            . AmazonAutoLinks_PluginUtility::getButton(
-                $iPostID
-            );
+            . AmazonAutoLinks_PluginUtility::getButton( $iPostID );
     }
         
 }
