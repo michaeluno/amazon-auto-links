84f814a72787f4a1ed253eb693ea7a3cb2abb0fb
Michael Uno <michael2@michaeluno.jp>
2022-03-15 22:04:41 +0900

    Tweak: Remove redundant container elements for button preview iframe elements

--- include/core/component/button/button_type/image/admin/form_field/AmazonAutoLinks_Button_Image_FormFields_Preview.php
+++ include/core/component/button/button_type/image/admin/form_field/AmazonAutoLinks_Button_Image_FormFields_Preview.php
@@ -18,17 +18,13 @@ class AmazonAutoLinks_Button_Image_FormFields_Preview extends AmazonAutoLinks_Fo
             array(
                 'field_id'          => $sFieldIDPrefix . '_button_preview',
                 'show_title_column' => false,
-                'content'           => "<div style='margin: 3em 3em 3em 0; width:100%;'>"
-                    . "<div style='margin-left: auto; margin-right: auto; '>" // text-align:center;
-                        . AmazonAutoLinks_Button_Utility::getIframeButtonPreview(
-                            $this->getHTTPQueryGET( array( 'post' ), '___button_id___' ),
-                            'image',
-                            '',
-                            array(),
-                            array( 'id' => 'button-image-preview', )
-                        )
-                    . "</div>"
-                . "</div>",
+                'content'           => AmazonAutoLinks_Button_Utility::getIframeButtonPreview(
+                    $this->getHTTPQueryGET( array( 'post' ), '___button_id___' ),
+                    'image',
+                    '',
+                    array(),
+                    array( 'id' => 'button-image-preview', )
+                ),
             )
         );
     }
