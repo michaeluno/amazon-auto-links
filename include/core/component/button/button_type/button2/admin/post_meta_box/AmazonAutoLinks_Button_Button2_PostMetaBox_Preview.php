7acafb7f44f2b5c603ebab09b7e768d5bd3278c8
Michael Uno <michael2@michaeluno.jp>
2022-03-16 23:34:58 +0900

    Refactor: Reduce code by using an extended property

--- include/core/component/button/button_type/button2/admin/post_meta_box/AmazonAutoLinks_Button_Button2_PostMetaBox_Preview.php
+++ include/core/component/button/button_type/button2/admin/post_meta_box/AmazonAutoLinks_Button_Button2_PostMetaBox_Preview.php
@@ -5,20 +5,21 @@
  * Generates links of Amazon products just coming out today. You just pick categories and they appear even in JavaScript disabled browsers.
  *
  * https://en.michaeluno.jp/amazon-auto-links/
- * Copyright (c) 2013-2021 Michael Uno
+ * Copyright (c) 2013-2022 Michael Uno
  */
 
 /**
  * Defines the meta box that shows a button preview.
+ * @since 5.2.0
  */
 class AmazonAutoLinks_Button_Button2_PostMetaBox_Preview extends AmazonAutoLinks_Button_Button2_PostMetaBox_Base {
 
-    public function setUp() {
+    /**
+     * @var   string[]
+     * @since 5.2.0
+     */
+    protected $_aFieldClasses = array(
+        'AmazonAutoLinks_Button_Button2_FormFields_Preview',
+    );
 
-        $_oFields = new AmazonAutoLinks_Button_Button2_FormFields_Preview( $this );
-        foreach( $_oFields->get() as $_aField ) {
-            $this->addSettingFields( $_aField );
-        }
-
-    }
 }
