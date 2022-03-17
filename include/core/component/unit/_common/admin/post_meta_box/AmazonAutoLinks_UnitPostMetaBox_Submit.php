2e4dc75371539c53c016afadd0bf3b22d09bc7bc
Michael Uno <michael2@michaeluno.jp>
2022-03-17 19:07:35 +0900

    Fix: Remove unnecessary translation items by calling them with wrapper methods
    This way, translation parser programs do not detect them so translators will have less translation items.

--- include/core/component/unit/_common/admin/post_meta_box/AmazonAutoLinks_UnitPostMetaBox_Submit.php
+++ include/core/component/unit/_common/admin/post_meta_box/AmazonAutoLinks_UnitPostMetaBox_Submit.php
@@ -5,7 +5,7 @@
  * Generates links of Amazon products just coming out today. You just pick categories and they appear even in JavaScript disabled browsers.
  *
  * https://en.michaeluno.jp/amazon-auto-links/
- * Copyright (c) 2013-2021 Michael Uno
+ * Copyright (c) 2013-2022 Michael Uno
  */
 
 /**
@@ -13,8 +13,14 @@
  */
 class AmazonAutoLinks_UnitPostMetaBox_Submit extends AmazonAutoLinks_UnitPostMetaBox_Base {
 
+    /**
+     * @var AmazonAutoLinks_PluginUtility
+     */
+    public $oUtil;
+
     public function setUp() {
         add_action( "do_" . $this->oProp->sClassName, array( $this, 'replyToPrintMetaBoxContent' ) );
+        $this->oUtil = new AmazonAutoLinks_PluginUtility;
     }
         
     public function _replyToRegisterMetaBoxes() {
@@ -62,25 +68,25 @@ class AmazonAutoLinks_UnitPostMetaBox_Submit extends AmazonAutoLinks_UnitPostMet
 
         <div id="misc-publishing-actions">
             <div class="misc-pub-section misc-pub-post-status">
-                <?php _e( 'Status:' ); ?>
+                <?php $this->oUtil->__e( 'Status:' ); ?>
                 <span id="post-status-display">
                     <?php
                     switch ( $post->post_status ) {
                         case 'private':
-                            _e( 'Privately Published' );
+                            $this->oUtil->__e( 'Privately Published' );
                             break;
                         case 'publish':
-                            _e( 'Published' );
+                            $this->oUtil->__e( 'Published' );
                             break;
                         case 'future':
-                            _e( 'Scheduled' );
+                            $this->oUtil->__e( 'Scheduled' );
                             break;
                         case 'pending':
-                            _e( 'Pending Review' );
+                            $this->oUtil->__e( 'Pending Review' );
                             break;
                         case 'draft':
                         case 'auto-draft':
-                            _e( 'Draft' );
+                            $this->oUtil->__e( 'Draft' );
                             break;
                     }
                     ?>
@@ -93,28 +99,28 @@ class AmazonAutoLinks_UnitPostMetaBox_Submit extends AmazonAutoLinks_UnitPostMet
                         $private_style = 'style="display:none"';
                     }
                     ?>
-                    <a href="#post_status" <?php echo $private_style; ?> class="edit-post-status hide-if-no-js" role="button"><span aria-hidden="true"><?php _e( 'Edit' ); ?></span> <span class="screen-reader-text"><?php _e( 'Edit status' ); ?></span></a>
+                    <a href="#post_status" <?php echo $private_style; ?> class="edit-post-status hide-if-no-js" role="button"><span aria-hidden="true"><?php $this->oUtil->__e( 'Edit' ); ?></span> <span class="screen-reader-text"><?php $this->oUtil->__e( 'Edit status' ); ?></span></a>
 
                     <div id="post-status-select" class="hide-if-js">
                         <input type="hidden" name="hidden_post_status" id="hidden_post_status" value="<?php echo esc_attr( ( 'auto-draft' === $post->post_status ) ? 'draft' : $post->post_status ); ?>" />
-                        <label for="post_status" class="screen-reader-text"><?php _e( 'Set status' ); ?></label>
+                        <label for="post_status" class="screen-reader-text"><?php $this->oUtil->__e( 'Set status' ); ?></label>
                         <select name="post_status" id="post_status">
                             <?php if ( 'publish' === $post->post_status ) : ?>
-                                <option<?php selected( $post->post_status, 'publish' ); ?> value='publish'><?php _e( 'Published' ); ?></option>
+                                <option<?php selected( $post->post_status, 'publish' ); ?> value='publish'><?php $this->oUtil->__e( 'Published' ); ?></option>
                             <?php elseif ( 'private' === $post->post_status ) : ?>
-                                <option<?php selected( $post->post_status, 'private' ); ?> value='publish'><?php _e( 'Privately Published' ); ?></option>
+                                <option<?php selected( $post->post_status, 'private' ); ?> value='publish'><?php $this->oUtil->__e( 'Privately Published' ); ?></option>
                             <?php elseif ( 'future' === $post->post_status ) : ?>
-                                <option<?php selected( $post->post_status, 'future' ); ?> value='future'><?php _e( 'Scheduled' ); ?></option>
+                                <option<?php selected( $post->post_status, 'future' ); ?> value='future'><?php $this->oUtil->__e( 'Scheduled' ); ?></option>
                             <?php endif; ?>
-                                <option<?php selected( $post->post_status, 'pending' ); ?> value='pending'><?php _e( 'Pending Review' ); ?></option>
+                                <option<?php selected( $post->post_status, 'pending' ); ?> value='pending'><?php $this->oUtil->__e( 'Pending Review' ); ?></option>
                             <?php if ( 'auto-draft' === $post->post_status ) : ?>
-                                <option<?php selected( $post->post_status, 'auto-draft' ); ?> value='draft'><?php _e( 'Draft' ); ?></option>
+                                <option<?php selected( $post->post_status, 'auto-draft' ); ?> value='draft'><?php $this->oUtil->__e( 'Draft' ); ?></option>
                             <?php else : ?>
-                                <option<?php selected( $post->post_status, 'draft' ); ?> value='draft'><?php _e( 'Draft' ); ?></option>
+                                <option<?php selected( $post->post_status, 'draft' ); ?> value='draft'><?php $this->oUtil->__e( 'Draft' ); ?></option>
                             <?php endif; ?>
                         </select>
-                        <a href="#post_status" class="save-post-status hide-if-no-js button"><?php _e( 'OK' ); ?></a>
-                        <a href="#post_status" class="cancel-post-status hide-if-no-js button-cancel"><?php _e( 'Cancel' ); ?></a>
+                        <a href="#post_status" class="save-post-status hide-if-no-js button"><?php $this->oUtil->__e( 'OK' ); ?></a>
+                        <a href="#post_status" class="cancel-post-status hide-if-no-js button-cancel"><?php $this->oUtil->__e( 'Cancel' ); ?></a>
                     </div>
                     <?php
                 }
@@ -122,22 +128,22 @@ class AmazonAutoLinks_UnitPostMetaBox_Submit extends AmazonAutoLinks_UnitPostMet
             </div>
 
             <div class="misc-pub-section misc-pub-visibility" id="visibility">
-                <?php _e( 'Visibility:' ); ?>
+                <?php $this->oUtil->__e( 'Visibility:' ); ?>
                 <span id="post-visibility-display">
                     <?php
                     if ( 'private' === $post->post_status ) {
                         $post->post_password = '';
                         $visibility          = 'private';
-                        $visibility_trans    = __( 'Private' );
+                        $visibility_trans    = $this->oUtil->___( 'Private' );
                     } elseif ( ! empty( $post->post_password ) ) {
                         $visibility       = 'password';
-                        $visibility_trans = __( 'Password protected' );
+                        $visibility_trans = $this->oUtil->___( 'Password protected' );
                     } elseif ( 'post' === $post_type && is_sticky( $post_id ) ) {
                         $visibility       = 'public';
-                        $visibility_trans = __( 'Public, Sticky' );
+                        $visibility_trans = $this->oUtil->___( 'Public, Sticky' );
                     } else {
                         $visibility       = 'public';
-                        $visibility_trans = __( 'Public' );
+                        $visibility_trans = $this->oUtil->___( 'Public' );
                     }
 
                     echo esc_html( $visibility_trans );
@@ -145,27 +151,27 @@ class AmazonAutoLinks_UnitPostMetaBox_Submit extends AmazonAutoLinks_UnitPostMet
                 </span>
 
                 <?php if ( $can_publish ) { ?>
-                    <a href="#visibility" class="edit-visibility hide-if-no-js" role="button"><span aria-hidden="true"><?php _e( 'Edit' ); ?></span> <span class="screen-reader-text"><?php _e( 'Edit visibility' ); ?></span></a>
+                    <a href="#visibility" class="edit-visibility hide-if-no-js" role="button"><span aria-hidden="true"><?php $this->oUtil->__e( 'Edit' ); ?></span> <span class="screen-reader-text"><?php $this->oUtil->__e( 'Edit visibility' ); ?></span></a>
 
                     <div id="post-visibility-select" class="hide-if-js">
                         <?php if ( 'post' === $post_type ) : ?>
                             <input type="checkbox" style="display:none" name="hidden_post_sticky" id="hidden-post-sticky" value="sticky" <?php checked( is_sticky( $post_id ) ); ?> />
                         <?php endif; ?>
 
-                        <input type="radio" name="visibility" id="visibility-radio-public" value="public" <?php checked( $visibility, 'public' ); ?> /> <label for="visibility-radio-public" class="selectit"><?php _e( 'Public' ); ?></label><br />
+                        <input type="radio" name="visibility" id="visibility-radio-public" value="public" <?php checked( $visibility, 'public' ); ?> /> <label for="visibility-radio-public" class="selectit"><?php $this->oUtil->__e( 'Public' ); ?></label><br />
 
                         <?php if ( 'post' === $post_type && current_user_can( 'edit_others_posts' ) ) : ?>
-                            <span id="sticky-span"><input id="sticky" name="sticky" type="checkbox" value="sticky" <?php checked( is_sticky( $post_id ) ); ?> /> <label for="sticky" class="selectit"><?php _e( 'Stick this post to the front page' ); ?></label><br /></span>
+                            <span id="sticky-span"><input id="sticky" name="sticky" type="checkbox" value="sticky" <?php checked( is_sticky( $post_id ) ); ?> /> <label for="sticky" class="selectit"><?php $this->oUtil->__e( 'Stick this post to the front page' ); ?></label><br /></span>
                         <?php endif; ?>
 
-                        <input type="radio" name="visibility" id="visibility-radio-password" value="password" <?php checked( $visibility, 'password' ); ?> /> <label for="visibility-radio-password" class="selectit"><?php _e( 'Password protected' ); ?></label><br />
-                        <span id="password-span"><label for="post_password"><?php _e( 'Password:' ); ?></label> <input type="text" name="post_password" id="post_password" value="<?php echo esc_attr( $post->post_password ); ?>"  maxlength="255" /><br /></span>
+                        <input type="radio" name="visibility" id="visibility-radio-password" value="password" <?php checked( $visibility, 'password' ); ?> /> <label for="visibility-radio-password" class="selectit"><?php $this->oUtil->__e( 'Password protected' ); ?></label><br />
+                        <span id="password-span"><label for="post_password"><?php $this->oUtil->__e( 'Password:' ); ?></label> <input type="text" name="post_password" id="post_password" value="<?php echo esc_attr( $post->post_password ); ?>"  maxlength="255" /><br /></span>
 
-                        <input type="radio" name="visibility" id="visibility-radio-private" value="private" <?php checked( $visibility, 'private' ); ?> /> <label for="visibility-radio-private" class="selectit"><?php _e( 'Private' ); ?></label><br />
+                        <input type="radio" name="visibility" id="visibility-radio-private" value="private" <?php checked( $visibility, 'private' ); ?> /> <label for="visibility-radio-private" class="selectit"><?php $this->oUtil->__e( 'Private' ); ?></label><br />
 
                         <p>
-                            <a href="#visibility" class="save-post-visibility hide-if-no-js button"><?php _e( 'OK' ); ?></a>
-                            <a href="#visibility" class="cancel-post-visibility hide-if-no-js button-cancel"><?php _e( 'Cancel' ); ?></a>
+                            <a href="#visibility" class="save-post-visibility hide-if-no-js button"><?php $this->oUtil->__e( 'OK' ); ?></a>
+                            <a href="#visibility" class="cancel-post-visibility hide-if-no-js button-cancel"><?php $this->oUtil->__e( 'Cancel' ); ?></a>
                         </p>
                     </div>
                 <?php } ?>
@@ -173,26 +179,26 @@ class AmazonAutoLinks_UnitPostMetaBox_Submit extends AmazonAutoLinks_UnitPostMet
 
             <?php
             /* translators: Publish box date string. 1: Date, 2: Time. See https://www.php.net/manual/datetime.format.php */
-            $date_string = __( '%1$s at %2$s' );
+            $date_string = $this->oUtil->___( '%1$s at %2$s' );
             /* translators: Publish box date format, see https://www.php.net/manual/datetime.format.php */
-            $date_format = _x( 'M j, Y', 'publish box date format' );
+            $date_format = $this->oUtil->__x( 'M j, Y', 'publish box date format' );
             /* translators: Publish box time format, see https://www.php.net/manual/datetime.format.php */
-            $time_format = _x( 'H:i', 'publish box time format' );
+            $time_format = $this->oUtil->__x( 'H:i', 'publish box time format' );
 
             if ( 'future' === $post->post_status ) { // Scheduled for publishing at a future date.
                 /* translators: Post date information. %s: Date on which the post is currently scheduled to be published. */
-                $stamp = __( 'Scheduled for: %s' );
+                $stamp = $this->oUtil->___( 'Scheduled for: %s' );
             } elseif ( 'publish' === $post->post_status || 'private' === $post->post_status ) { // Already published.
                 /* translators: Post date information. %s: Date on which the post was published. */
-                $stamp = __( 'Published on: %s' );
+                $stamp = $this->oUtil->___( 'Published on: %s' );
             } elseif ( '0000-00-00 00:00:00' === $post->post_date_gmt ) { // Draft, 1 or more saves, no date specified.
-                $stamp = __( 'Publish <b>immediately</b>' );
+                $stamp = $this->oUtil->___( 'Publish <b>immediately</b>' );
             } elseif ( time() < strtotime( $post->post_date_gmt . ' +0000' ) ) { // Draft, 1 or more saves, future date specified.
                 /* translators: Post date information. %s: Date on which the post is to be published. */
-                $stamp = __( 'Schedule for: %s' );
+                $stamp = $this->oUtil->___( 'Schedule for: %s' );
             } else { // Draft, 1 or more saves, date specified.
                 /* translators: Post date information. %s: Date on which the post is to be published. */
-                $stamp = __( 'Publish on: %s' );
+                $stamp = $this->oUtil->___( 'Publish on: %s' );
             }
             $date = sprintf(
                 $date_string,
@@ -207,11 +213,11 @@ class AmazonAutoLinks_UnitPostMetaBox_Submit extends AmazonAutoLinks_UnitPostMet
                         <?php printf( $stamp, '<b>' . $date . '</b>' ); ?>
                     </span>
                     <a href="#edit_timestamp" class="edit-timestamp hide-if-no-js" role="button">
-                        <span aria-hidden="true"><?php _e( 'Edit' ); ?></span>
-                        <span class="screen-reader-text"><?php _e( 'Edit date and time' ); ?></span>
+                        <span aria-hidden="true"><?php $this->oUtil->__e( 'Edit' ); ?></span>
+                        <span class="screen-reader-text"><?php $this->oUtil->__e( 'Edit date and time' ); ?></span>
                     </a>
                     <fieldset id="timestampdiv" class="hide-if-js">
-                        <legend class="screen-reader-text"><?php _e( 'Date and time' ); ?></legend>
+                        <legend class="screen-reader-text"><?php $this->oUtil->__e( 'Date and time' ); ?></legend>
                         <?php touch_time( ( 'edit' === $action ), 1 ); ?>
                     </fieldset>
                 </div>
@@ -225,7 +231,7 @@ class AmazonAutoLinks_UnitPostMetaBox_Submit extends AmazonAutoLinks_UnitPostMet
                         <?php
                         printf(
                             /* translators: %s: URL to the Customizer. */
-                            __( 'This draft comes from your <a href="%s">unpublished customization changes</a>. You can edit, but there&#8217;s no need to publish now. It will be published automatically with those changes.' ),
+                            $this->oUtil->___( 'This draft comes from your <a href="%s">unpublished customization changes</a>. You can edit, but there&#8217;s no need to publish now. It will be published automatically with those changes.' ),
                             esc_url(
                                 add_query_arg(
                                     'changeset_uuid',
@@ -248,7 +254,7 @@ class AmazonAutoLinks_UnitPostMetaBox_Submit extends AmazonAutoLinks_UnitPostMet
         <div id="delete-action">
             <?php
             if ( current_user_can( 'delete_post', $post_id ) ) {
-                $delete_text = EMPTY_TRASH_DAYS ? __( 'Move to Trash' ) : __( 'Delete permanently' );
+                $delete_text = EMPTY_TRASH_DAYS ? $this->oUtil->___( 'Move to Trash' ) : $this->oUtil->___( 'Delete permanently' );
                 ?>
                 <a class="submitdelete deletion" href="<?php echo get_delete_post_link( $post_id ); ?>"><?php echo esc_html( $delete_text ); ?></a>
                 <?php
@@ -257,7 +263,7 @@ class AmazonAutoLinks_UnitPostMetaBox_Submit extends AmazonAutoLinks_UnitPostMet
         </div>
         <div id="publishing-action">
             <span class="spinner"></span>
-            <span id="publish" class="button button-primary button-large disabled amazon-auto-links-form-tooltip" ><?php esc_html_e( 'Update' ); ?>
+            <span id="publish" class="button button-primary button-large disabled amazon-auto-links-form-tooltip" ><?php $this->oUtil->_esc_html_e( 'Update' ); ?>
                 <span class="amazon-auto-links-form-tooltip-content hidden">
                     <span class="dashicons dashicons-warning field-error"></span>
                     <span><?php echo wp_kses( AmazonAutoLinks_Message::getUpgradePromptMessage(), 'post' ); ?></span>
