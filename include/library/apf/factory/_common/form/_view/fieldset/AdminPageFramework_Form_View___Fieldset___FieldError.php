621cffbf2ce060ad2134d74fcc1e867b0c5aa893
Michael Uno <michael2@michaeluno.jp>
2022-03-31 16:09:27 +0900

    Fix: Fix a bug that caused the "Maximum Call Stack Size Exceeded" JavaScript error when clearing a color field input
    By updating Admin Page Framework to 3.9.0b04.
    
    This also fixes an issue that some input change events are not triggered.

--- include/library/apf/factory/_common/form/_view/fieldset/AdminPageFramework_Form_View___Fieldset___FieldError.php
+++ include/library/apf/factory/_common/form/_view/fieldset/AdminPageFramework_Form_View___Fieldset___FieldError.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Admin Page Framework v3.9.1b03 by Michael Uno
+ * Admin Page Framework v3.9.1b04 by Michael Uno
  * Compiled with Admin Page Framework Compiler <https://github.com/michaeluno/amazon-auto-links-compiler>
  * <https://en.michaeluno.jp/amazon-auto-links>
  * Copyright (c) 2013-2022, Michael Uno; Licensed under MIT <https://opensource.org/licenses/MIT>
