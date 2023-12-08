851571ad6453963d4cc9008ba7fc273bb4acd8ea
Michael Uno <michael@michaeluno.jp>
2023-12-08 14:34:01 +0900

    Fix: Fix an issue that sometimes API responses are empty
    By omitting the InstanceId parameter.
    
    Also, introduce the logic of retrying.

--- include/core/_common/utility/interpreter/ad-widget-api/AmazonAutoLinks_AdWidgetAPI_Base.php
+++ include/core/_common/utility/interpreter/ad-widget-api/AmazonAutoLinks_AdWidgetAPI_Base.php
@@ -46,12 +46,29 @@ class AmazonAutoLinks_AdWidgetAPI_Base extends AmazonAutoLinks_PluginUtility {
      * @since  4.6.9
      */
     public function getResponse( $sEndpoint ) {
+
         $_aArguments = $this->aHTTPArguments + array(
             'user-agent' => 'WordPress/' . $GLOBALS[ 'wp_version' ],
             'timeout'    => 29,
         );
-        $_oHTTP      = new AmazonAutoLinks_HTTPClient( $sEndpoint, $this->iCacheDuration, $_aArguments, 'ad_widget_api' );
-        return $_oHTTP->getBody();
+
+        // [5.3.6+] Sometimes, the API returns an empty response with the status code 200. If that happens, retry
+        $_sHTTPBody = '';
+        $_iAttempts = 0;
+        $_iMax      = 30;
+        While ( $_iAttempts <= $_iMax ) {
+            $_oHTTP      = new AmazonAutoLinks_HTTPClient( $sEndpoint, $this->iCacheDuration, $_aArguments, 'ad_widget_api' );
+            $_sHTTPBody  = $_oHTTP->getBody();
+            if ( 200 !== $_oHTTP->getStatusCode() || '' !== $_sHTTPBody ) {
+                break;
+            }
+            $_iAttempts++;
+            $_aArguments[ 'renew_cache' ] = true;
+            usleep( 500 );
+        }
+
+        return $_sHTTPBody;
+
     }
 
     /**
@@ -69,6 +86,9 @@ class AmazonAutoLinks_AdWidgetAPI_Base extends AmazonAutoLinks_PluginUtility {
         preg_match( "/[^(]*\((.*)\)/", $sJSONP, $_aMatches );
         $_sJSONJS   = isset( $_aMatches[ 1 ] ) ? $_aMatches[ 1 ] : '';
 
+        // Sanitize the text which prevents the below code from converting it
+        $_sJSONJS   = self::___getJSONPSanitized( $_sJSONJS );
+
         // The JSON syntax is still JS based. Enclose keys with double quotes
         // @see https://stackoverflow.com/a/40326949
         // (?<!\\\)" <-- this avoids matching \" which is escaped double quotes
@@ -77,5 +97,22 @@ class AmazonAutoLinks_AdWidgetAPI_Base extends AmazonAutoLinks_PluginUtility {
         return json_decode( $_sJSON, true );
 
     }
+
+        /**
+         * @param  $sRawJSONP
+         * @return string
+         * @since  5.3.6
+         */
+        static private function ___getJSONPSanitized( $sRawJSONP ) {
+
+            // When the endpoint omits the InstanceId parameter, JavaScript code is included in the JSON element in the response
+            // e.g. ..." } ], MarketPlace: "DE", InstanceId: "$toolsFactory.first($params.get("InstanceId"))"})
+            return str_replace(
+                ', InstanceId: "$toolsFactory.first($params.get("InstanceId"))"',
+                '',
+                $sRawJSONP
+            );
+
+        }
 
 }
